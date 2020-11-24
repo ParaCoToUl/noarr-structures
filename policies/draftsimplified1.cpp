5f8fe08176faf7e9e9e6f1e597aed1c7e0d9f318
jiriklepl <jiriklepl@seznam.cz>
2020-11-24 23:07:03 +0100

    fixed, simplified

--- policies/draftsimplified1.cpp
+++ policies/draftsimplified1.cpp
@@ -1,27 +1,21 @@
-struct cross;
-
 template<unsigned length> struct array;
 template<typename ...Ts> struct shape;
+struct cross;
 
 template<typename T, typename... Ts, unsigned n>
 struct shape<T, array<n>, cross, Ts...> {
-private:
     using sub_shape = shape<T, Ts...>;
-
-public:
     static constexpr unsigned size = sub_shape::size * n;
 
     template<typename index, typename ...indices>
-    static T &get(T *memory, index first, indices... rest) {
+    static T &at(T *memory, index first, indices... rest) {
         return sub_shape::at(
-            memory + (sub_shape::size * first),
-            rest...);
+            memory + (sub_shape::size * first), rest...);
     }
 };
 
 template<typename T, unsigned n>
 struct shape<T, array<n>> {
-public:
     static constexpr unsigned size = n;
 
     template<typename index>
