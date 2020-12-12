d557c2640c47c05d2e0efd4e4d956ce74c1b90f1
jiriklepl <jiriklepl@seznam.cz>
2020-12-12 06:02:36 +0100

    made index-safe;  vector broken

--- policies/prototype/arrr.cpp
+++ policies/prototype/arrr.cpp
@@ -1,102 +1,217 @@
 #include <iostream>
 #include <cassert>
 
-using namespace std;
 namespace arrr{
 
-/* indexers */
+using std::size_t;
 
-template<typename ARR, char C, char... CS>
+// really, someone wants to be explained what an array is?
+template<size_t N, typename T, char IDX = '\0'>
+struct array;
+
+// vector: array with size given at runtime
+template<typename T, char IDX = '\0'>
+struct vector;
+
+// vector which has been given a size
+template<typename T, char IDX = '\0'>
+struct sized_vector;
+
+// for indexed structures already provided with an index
+template<typename T>
+struct offset;
+
+// for ground types
+template<typename T>
+struct scalar;
+
+// struct tuple;
+
+// helpers:
+
+template<typename T, T first, T... rest>
+struct is_distinct;
+
+template<typename T, typename, T first, T... rest>
+struct unique_values_help { static constexpr bool value = false; };
+
+template<typename T, T first, T... rest>
+struct unique_values_help<T, typename std::enable_if<is_distinct<T, first, rest...>::value>::type, first, rest...> { static constexpr bool value = true; };
+
+template<typename T, T first, T second, T... rest>
+struct is_distinct<T, first, second, rest...> : is_distinct<T, first, rest...> {};
+
+template<typename T, T first, T... rest>
+struct is_distinct<T, first, first, rest...> { static constexpr bool value = false; };
+
+template<typename T, T first, T second>
+struct is_distinct<T, first, second> { static constexpr bool value = true; };
+
+template<typename T, T first>
+struct is_distinct<T, first, first> { static constexpr bool value = false; };
+
+template<typename T, T first, T... rest>
+using unique_values = unique_values_help<T, void, first, rest...>;
+
+template<typename T>
+struct is_scalar { static constexpr bool value = false; };
+
+template<typename T>
+struct is_scalar<scalar<T>> { static constexpr bool value = true; };
+
+/* indexer */
+
+template<typename S, char C, char... CS>
 struct idx_t {
-	template<typename... Args>
-	constexpr size_t operator()(ARR a, size_t i, Args... args) {
-		idx_t<ARR, C> curr;
-		idx_t<ARR, CS...> next;
-		return curr(a, i) + next(a, args...);
-	};
+	static_assert(unique_values<char, C, CS...>::value, "indexes collide");
+	static_assert(S::dynamic_index(), "structure does not provide dynamic indexing"); // TODO: dynamic or linear?
+	using type = S;
+	const S &s;
+	template<typename Arg, typename... Args>
+	constexpr size_t operator()(Arg arg, Args... args) const {
+		static_assert(S::template has_idx<C>(), "index not found");
+		static_assert(sizeof...(CS) == sizeof...(Args), "indexes given do not match indexes declared");
+		if constexpr (S::IDX == C) {
+			return s.get(arg) + s.template idx1<CS...>(arg)(args...);
+		} else {
+			return idx_t<S, CS..., C>{s}(args..., arg);
+		}
+	}
 };
 
-template<typename ARR, char C>
-struct idx_t<ARR,C> {
-	constexpr size_t operator()(ARR a, size_t i) const {
-		static_assert(a.template has_idx<C>(), "index not found");
-		return a.template idx1<C>(i);
+// TODO: add calling idx on idx_t?
+// TODO: binding values for various indexes
+
+template<typename S, char C>
+struct idx_t<S, C> {
+	static_assert(S::dynamic_index(), "structure does not provide dynamic indexing"); // TODO: same as above
+	const S &s;
+	template<typename... Args>
+	constexpr size_t operator()(Args...) const {
+		static_assert(1 == sizeof...(Args), "indexes given do not match indexes declared");
+		return 0xDEAD; // FIXME
+	}
+	template<typename Arg>
+	constexpr size_t operator()(Arg arg) const {
+		static_assert(S::IDX == C, "index does not match");
+		static_assert(is_scalar<typename S::item_t>::value, "path does not lead to ground item");
+		return s.get(arg);
 	}
 };
 
 /* data types */
 
-template<char IDX, size_t N, typename T>
-struct array {
+template<size_t N, typename T, char I>
+struct array  {
 	T t;
-	constexpr size_t step() {return t.size();}
-	constexpr size_t size() {return N*step();}
-
-	template<char C>
-	constexpr bool has_idx() {
-		if constexpr (IDX==C) return true;
-		return t.template has_idx<C>();
-	}
-
-	template<char C>
-	constexpr size_t idx1(size_t i) {
-		if constexpr (IDX==C) return i*step();
-		else return t.template idx1<C>(i);
-	}
+	static constexpr char IDX = I;
 	
-	template<char... CS>
-	constexpr idx_t<array<IDX,N,T>, CS...> idx(){
-		return idx_t<array<IDX,N,T>, CS...>();
+	static constexpr bool dynamic() { return false; }
+	static constexpr bool dynamic_index() { return true; }
+	using item_t = T;
+
+	constexpr size_t step() const { return t.size(); }
+	constexpr size_t size() const { return N * step(); }
+
+	template<size_t i>
+	constexpr size_t get() const {
+		return i * step();
 	}
 
-  template<char C>
-  void resize(size_t s) {
-    static_assert(IDX!=C, "trying to resize an array");
-    t.template resize<C>(s);
-  }
+	constexpr size_t get(size_t i) const {
+		return i * step();
+	}
+
+	template<size_t i, char... CS>
+	constexpr idx_t<T, CS...> idx1() const {
+		return idx_t<T, CS...>{t};
+	}
+
+	template<char... CS>
+	constexpr idx_t<T, CS...> idx1(size_t i) const {
+		return idx_t<T, CS...>{t};
+	}
+
+	template<char C>
+	static constexpr bool has_idx() {
+		if (C == IDX) return true;
+		else return T::template has_idx<C>();
+	}
+
+	template<char... CS>
+	constexpr idx_t<array, CS...> idx() const {
+		return idx_t<array, CS...>{*this};
+	}
+
+	template<char C>
+	void resize(size_t s) {
+		static_assert(IDX != C, "trying to resize an array");
+		t.template resize<C>(s);
+	}
 };
 
-template<char IDX, typename T>
-struct vector{
-  T t;
-  size_t n;
+template<typename T, char I>
+struct vector {
+	T t;
+	size_t n;
+	static constexpr char IDX = I;
+	
+	static constexpr bool dynamic() { return true; }
+	static constexpr bool dynamic_index() { return true; }
+	using item_t = T;
 
-  constexpr size_t step() {return t.size();}
-  constexpr size_t size() {return n*step();}
+	constexpr size_t step() const { return t.size(); }
+	constexpr size_t size() const { /* TODO */ return n * step(); }
 
-	template<char C>
-	constexpr bool has_idx() {
-		if constexpr (IDX==C) return true;
-		return t.template has_idx<C>();
+	template<size_t i>
+	constexpr size_t get() const {
+		return i * step();
 	}
 
+	constexpr size_t get(size_t i) const {
+		return i * step();
+	}
+
+	template<size_t i, char... CS>
+	constexpr idx_t<T, CS...> idx1() const {
+		return idx_t<T, CS...>{t};
+	}
+
+	template<char... CS>
+	constexpr idx_t<T, CS...> idx1(size_t i) const {
+		return idx_t<T, CS...>{t};
+	}
+	
 	template<char C>
-	constexpr size_t idx1(size_t i) {
-		if constexpr (IDX==C) return i*step();
-		else return t.template idx1<C>(i);
+	static constexpr bool has_idx() {
+		if (C == IDX) return true;
+		else return T::template has_idx<C>();
 	}
 	
 	template<char... CS>
-	constexpr idx_t<vector<IDX,T>, CS...> idx(){
-		return idx_t<vector<IDX,T>, CS...>();
+	constexpr idx_t<vector, CS...> idx() const {
+		return idx_t<vector, CS...>{*this};
 	}
 
-  template<char C>
-  void resize(size_t s) {
-		if constexpr (IDX==C) n=s;
-    else t.template resize<C>(s);
-  }
+	template<char C>
+	void resize(size_t s) {
+		if constexpr (IDX == C) n = s;
+		else t.template resize<C>(s);
+	}
 };
 
 template<typename T>
 struct scalar {
-	static constexpr size_t step() {return sizeof(T);}
-	static constexpr size_t size() {return sizeof(T);}
+	using item_t = T;
+
+	static constexpr size_t step() { return sizeof(T); }
+	static constexpr size_t size() { return sizeof(T); }
 
 	template <char C>
 	static constexpr bool has_idx() {
 		return false;
 	}
+
 };
 
 } //namespace arrr
@@ -104,13 +219,16 @@ struct scalar {
 using namespace arrr;
 
 int main() {
-	array<'x', 10, vector<'y', scalar<float>>> a;
-	a.resize<'y'>(12300);
+	array<10, array<20,scalar<float>, 'y'>, 'x'> a;
 
-	std::cout << a.idx1<'x'>(3) << std::endl;
-	std::cout << a.idx1<'y'>(3) << std::endl;
+	{
+		auto idx = a.idx<'x', 'y'>();
+		std::cout << idx(1, 2) << std::endl;
+	}
 
-	auto idx = a.idx<'y','x'>();
-	std::cout << idx(a, 1,2) << std::endl;
+	{
+		auto idx = a.idx<'y', 'x'>();
+		std::cout << idx(2, 1) << std::endl;
+	}
 	return 0;
 }
