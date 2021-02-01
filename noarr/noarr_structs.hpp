0d92afb937b8e4fa136887684f7999b91e388520
jiriklepl <jiriklepl@seznam.cz>
2021-02-01 21:54:20 +0100

    renamed to noarr_structs, removed funcs

--- noarr/noarr_structs.hpp
+++ noarr/noarr_structs.hpp
@@ -1,5 +1,5 @@
-#ifndef NOARR_TYPES_HPP
-#define NOARR_TYPES_HPP
+#ifndef NOARR_STRUCTS_HPP
+#define NOARR_STRUCTS_HPP
 
 #include "noarr_core.hpp"
 
@@ -10,8 +10,8 @@ struct scalar {
     std::tuple<> sub_structures;
     static constexpr dims_impl<> dims = {};
     constexpr scalar() : sub_structures{} {}
-    static constexpr scalar<T> construct() {
-        return {};
+    static constexpr auto construct() {
+        return scalar<T>{};
     }
     static constexpr std::size_t size() { return sizeof(T); }
 };
@@ -21,10 +21,10 @@ struct vector {
     std::tuple<T> sub_structures;
     static constexpr dims_impl<DIM> dims = {};
     constexpr vector() : sub_structures{{}} {}
-    constexpr vector(T sub_structure) : sub_structures{std::make_tuple(sub_structure)} {}
+    explicit constexpr vector(T sub_structure) : sub_structures{std::make_tuple(sub_structure)} {}
     template<typename T2>
-    static constexpr vector<DIM, T2> construct(T2 sub_structure) {
-        return {sub_structure};
+    static constexpr auto construct(T2 sub_structure) {
+        return vector<DIM, T2>{sub_structure};
     }
 };
 
@@ -34,10 +34,10 @@ struct array {
     static constexpr std::size_t length = L;
     static constexpr dims_impl<DIM> dims = {};
     constexpr array() : sub_structures{{}} {}
-    constexpr array(T sub_structure) : sub_structures{std::make_tuple(sub_structure)} {}
+    explicit constexpr array(T sub_structure) : sub_structures{std::make_tuple(sub_structure)} {}
     template<typename T2>
-    static constexpr array<DIM, L, T2> construct(T2 sub_structure) {
-        return {sub_structure};
+    static constexpr auto construct(T2 sub_structure) {
+        return array<DIM, L, T2>{sub_structure};
     }
 
     constexpr std::size_t size() { return std::get<0>(sub_structures).size() * L; }
@@ -52,28 +52,14 @@ struct sized_vector : vector<DIM, T> {
     using vector<DIM, T>::sub_structures;
     constexpr sized_vector(T sub_structure, std::size_t length) : vector<DIM, T>{sub_structure}, length{length} {}
     template<typename T2>
-    constexpr sized_vector<DIM, T2> construct(T2 sub_structure) const {
-        return {sub_structure, length};
+    constexpr auto construct(T2 sub_structure) const {
+        return sized_vector<DIM, T2>{sub_structure, length};
     }
 
     constexpr std::size_t size() { return std::get<0>(sub_structures).size() * length; }
     constexpr std::size_t offset(std::size_t i) { return std::get<0>(sub_structures).size() * i; }
 };
 
-template<char DIM>
-struct resize {
-    std::size_t length;
-    constexpr resize(std::size_t length) : length{length} {}
-    template<typename T>
-    constexpr sized_vector<DIM, T> operator()(vector<DIM, T> v) const {
-        return {std::get<0>(v.sub_structures), length};
-    }
-    template<typename T>
-    constexpr sized_vector<DIM, T> operator()(sized_vector<DIM, T> v) const {
-        return {std::get<0>(v.sub_structures), length};
-    }
-};
-
 }
 
-#endif // NOARR_TYPES_HPP
+#endif // NOARR_STRUCTS_HPP
