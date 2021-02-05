bab14d3e0f96b18f55177dafd65f9a9c8c0335e8
jiriklepl <jiriklepl@seznam.cz>
2021-02-05 04:37:36 +0100

    added formating & some tests for tuples

--- noarr/main.cpp
+++ noarr/main.cpp
@@ -8,10 +8,12 @@ using namespace noarr;
 int main() {
     vector<'x', scalar<float>> v;
     array<'y', 20000, vector<'x', scalar<float>>> v2;
+    tuple<'t', array<'x', 10, scalar<float>>, array<'x', 10, scalar<int>>> t;
+
     auto vs = v % resize<'x'>{10}; // transform
     std::cout << "vs = v % resize<'x'>{10}: " << typeid(vs).name() << std::endl;
     std::cout << "vs.size(): " << vs.size() << std::endl;
-    std::cout << "sizeof(vs): " << sizeof(vs) << std::endl;
+    std::cout << "sizeof(vs): " << sizeof(vs) << std::endl << std::endl;
 
     auto vs2 = v2 % resize<'x'>{20}; // transform
     std::cout << "vs2 = v % resize<'x'>{10}: " << typeid(vs2).name() << std::endl;
@@ -22,12 +24,12 @@ int main() {
     std::cout << "vs2 % fixs<'x', 'y'>{5, 5}:" << typeid(vs2 % fixs<'x', 'y'>{5, 5}).name() << std::endl;
     std::cout << "vs2 % fixs<'x', 'y'>{5, 5} % offset{}:" << vs2 % fixs<'x', 'y'>{5, 5} % offset{} << std::endl;
     std::cout << "vs2 % fixs<'x', 'y'>{5, 5} % offset{}:" << (vs2 % fixs<'y', 'x'>{5, 5} % offset()) << std::endl;
-    std::cout << "vs2 % get_offset<'y'>{5}:" << (vs2 % get_offset<'y'>{5}) << std::endl;
+    std::cout << "vs2 % get_offset<'y'>{5}:" << (vs2 % get_offset<'y'>{5}) << std::endl << std::endl;
 
     auto vs3 = v2 % cresize<'x', 10>{}; // transform
     std::cout << "vs3 = v % cresize<'x', 10>{}: " << typeid(vs3).name() << std::endl;
     std::cout << "vs3.size(): " << vs3.size() << std::endl;
-    std::cout << "sizeof(vs3): " << sizeof(vs3) << std::endl;
+    std::cout << "sizeof(vs3): " << sizeof(vs3) << std::endl << std::endl;
 
     std::size_t l;
     std::cout << "choose l... ";
@@ -35,5 +37,8 @@ int main() {
     auto vs4 = pipe(v2, cresize<'y', 10>{}, resize<'x'>{l}); // transform
     std::cout << "vs4 = pipe(v2, cresize<'y', 10>{}, resize<'x'>{l}): " << typeid(vs4).name() << std::endl;
     std::cout << "vs4.size(): " << vs4.size() << std::endl;
-    std::cout << "sizeof(vs4): " << sizeof(vs4) << std::endl;
+    std::cout << "sizeof(vs4): " << sizeof(vs4) << std::endl << std::endl;
+
+    std::cout << "t.size(): " << t.size() << std::endl;
+    std::cout << "sizeof(t): " << sizeof(t) << std::endl;
 }
