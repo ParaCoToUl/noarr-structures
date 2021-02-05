f11e788e620ac46fdfd4010533f2efac3f778259
jiriklepl <jiriklepl@seznam.cz>
2021-02-05 04:39:01 +0100

    slightly changed the cuda test file

--- noarr/cuda.cu
+++ noarr/cuda.cu
@@ -7,7 +7,8 @@ using namespace noarr;
 // same body, two data layouts:
 template<typename AS>
 __global__ void kernel(float *data, AS as) {
-    *(float*)((char*)data + (as % fixs<'y', 'x'>(blockIdx.x, threadIdx.x) % offset())) = blockIdx.x * blockDim.x + threadIdx.x;
+    auto index = as % fixs<'y', 'x'>(blockIdx.x, threadIdx.x);
+    *(float*)((char*)data + (index % offset())) = blockIdx.x * blockDim.x + threadIdx.x;
 }
 
 __global__ void kernel_handmade(float *data, size_t size) {
@@ -23,15 +24,16 @@ int main() {
 
     // kernel<<<20000, 20>>>(data, (scalar<float> ^ vector<'y'> ^ array<'x', 20000>) % resize<'y'>(20));
     const auto av = array<'y', 20000, vector<'x', scalar<float>>>{};
-    const auto avr = av % resize<'x'>(20);
+    volatile std::size_t s = 20;
+    const auto avr = av % resize<'x'>(s);
     kernel<<<20000, 20>>>(data, avr);
     //kernel_handmade<<<20000, 20>>>(data, 20);
-
+    
     cudaMemcpy(local.data(), data, sizeof(local), cudaMemcpyDeviceToHost);
-
+    
     size_t i = 0;
     for (auto f : local) {
-        std::cout << f << ((i++ % 30 == 29) ? '\n' : ' ');
+        std::cout << f << ((i++ % 25 == 24) ? '\n' : ' ');
     }
 
     std::cout.flush();
