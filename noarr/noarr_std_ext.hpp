e45cc434020f2401be7770956ce34eff690e385c
jiriklepl <jiriklepl@seznam.cz>
2021-02-06 21:16:33 +0100

    removed empty_struct

--- noarr/noarr_std_ext.hpp
+++ noarr/noarr_std_ext.hpp
@@ -12,15 +12,6 @@ using void_t = void;
 template<class T>
 using remove_cvref = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
 
-// TODO: this is not really cosher
-struct empty_struct_t {
-    constexpr empty_struct_t() : _value{} {}
-    const char _value[0];
-};
-
-template <typename T>
-using is_empty = typename std::is_base_of<empty_struct_t, T>;
-
 template<class T>
 struct is_array {
     using value_type = bool;
