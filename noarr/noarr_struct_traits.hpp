db41c93ffdbcaf4fac2a3a5424608e7c717b9e4e
jiriklepl <jiriklepl@seznam.cz>
2021-02-14 04:47:11 +0100

    added structure traits

---
+++ noarr/noarr_struct_traits.hpp
@@ -0,0 +1,64 @@
+#ifndef NOARR_STRUCT_TRAITS_HPP
+#define NOARR_STRUCT_TRAITS_HPP
+
+#include <type_traits>
+
+#include "noarr_std_ext.hpp"
+#include "noarr_core.hpp"
+#include "noarr_struct_desc.hpp"
+
+namespace noarr {
+
+template<typename T, typename = void>
+struct _is_static_dimension {
+    using type = std::false_type;
+};
+
+template<typename T>
+struct _is_static_dimension<T, void_t<decltype(std::declval<T>().template offset<std::size_t{0}>())>> {
+    using type = std::true_type;
+};
+
+template<typename T, typename = void>
+struct _is_dynamic_dimension {
+    using type = std::false_type;
+};
+
+template<typename T, typename = void>
+struct _is_dynamic_dimension2 {
+    using type = std::false_type;
+};
+
+template<typename T>
+struct _is_dynamic_dimension<T, void_t<decltype(std::declval<T>().offset(std::declval<std::size_t>()))>> {
+    using type = std::true_type;
+};
+
+template<typename T>
+using is_static_dimension = typename _is_static_dimension<T>::type;
+
+template<typename T>
+using is_dynamic_dimension = typename _is_dynamic_dimension<T>::type;
+
+template<typename T>
+struct _is_point; // TODO
+
+template<typename T>
+using is_point = typename _is_point<T>::type;
+
+template<typename T, typename = void>
+struct _is_cube {
+    using type = std::false_type;
+};
+
+template<typename T>
+using is_cube = typename _is_cube<remove_cvref<T>>::type;
+
+template<typename T>
+struct _is_cube<T, std::enable_if_t<(is_dynamic_dimension<T>::value || std::is_same<typename get_struct_desc_t<T>::dims, dims_impl<>>::value) && tuple_forall<is_cube, typename sub_structures<T>::value_type>::value>> {
+    using type = std::true_type;
+};
+
+}
+
+#endif // NOARR_STRUCT_TRAITS_HPP
