0d5afcf76c7ba52a8be2d97b6cef26105114bc76
jiriklepl <jiriklepl@seznam.cz>
2021-02-23 19:32:52 +0100

    fixed

--- noarr/noarr_lifting.hpp
+++ noarr/noarr_lifting.hpp
@@ -12,7 +12,7 @@
 namespace noarr {
 
 template<typename Struct>
-auto lift(std::istream &in, Struct s, char *offset) -> std::enable_if_t<is_dynamic_dimension<Struct>::value, std::istream&>{
+inline constexpr auto lift(std::istream &in, Struct s, char *offset) -> std::enable_if_t<is_dynamic_dimension<Struct>::value, std::istream&>{
     for (std::size_t i = 0; i < s.length(); ++i) {
         lift(in, std::get<0>(s.sub_structures()), offset + s.offset(i));
     }
@@ -36,25 +36,22 @@ struct lift_static<0, Struct> {
 };
 
 template<typename Struct>
-auto lift(std::istream &in, Struct s, char *offset) -> std::enable_if_t<!is_dynamic_dimension<Struct>::value && is_static_dimension<Struct>::value, std::istream&>{
+inline constexpr auto lift(std::istream &in, Struct s, char *offset) -> std::enable_if_t<!is_dynamic_dimension<Struct>::value && is_static_dimension<Struct>::value, std::istream&>{
     return lift_static<s.length(), Struct>::lift_(in, s, offset);
 }
 
 template<typename Struct>
-auto lift(std::istream &in, Struct s, char *offset) -> std::enable_if_t<is_point<Struct>::value, std::istream&>{
+inline constexpr auto lift(std::istream &in, Struct s, char *offset) -> std::enable_if_t<is_point<Struct>::value, std::istream&>{
     return lift(in, std::get<0>(s.sub_structures()), offset + s.offset());
 }
 
 template<typename Type>
-std::istream &lift(std::istream &in, scalar<Type>, char *offset) {
+inline constexpr std::istream &lift(std::istream &in, scalar<Type>, char *offset) {
     return in >> *reinterpret_cast<Type*>(offset);
 }
 
-
-
-
 template<typename Struct>
-auto unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<is_dynamic_dimension<Struct>::value, std::ostream&>{
+inline constexpr auto unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<is_dynamic_dimension<Struct>::value, std::ostream&>{
     for (std::size_t i = 0; i < s.length(); ++i) {
         unlift(out, std::get<0>(s.sub_structures()), offset + s.offset(i));
     }
@@ -78,17 +75,17 @@ struct unlift_static<0, Struct> {
 };
 
 template<typename Struct>
-auto unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<!is_dynamic_dimension<Struct>::value && is_static_dimension<Struct>::value, std::ostream&>{
+inline constexpr auto unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<!is_dynamic_dimension<Struct>::value && is_static_dimension<Struct>::value, std::ostream&>{
     return unlift_static<s.length(), Struct>::unlift_(out, s, offset);
 }
 
 template<typename Struct>
-auto unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<is_point<Struct>::value, std::ostream&>{
+inline constexpr auto unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<is_point<Struct>::value, std::ostream&>{
     return unlift(out, std::get<0>(s.sub_structures()), offset + s.offset());
 }
 
 template<typename Type>
-std::ostream &unlift(std::ostream &out, scalar<Type>, char *offset) {
+inline constexpr std::ostream &unlift(std::ostream &out, scalar<Type>, char *offset) {
     return out << *reinterpret_cast<Type*>(offset);
 }
 
