8e749b97526be58d37adc8b40bb791d1901ba57d
jiriklepl <jiriklepl@seznam.cz>
2021-03-31 04:34:08 +0200

    added new dummy test

--- noarr_pipelines/tests/structures/dummy_test.cpp
+++ noarr_pipelines/tests/structures/dummy_test.cpp
@@ -1,6 +1,11 @@
 #include <catch2/catch.hpp>
 #include "noarr/structures.hpp"
 
+using namespace noarr;
+
 TEST_CASE("Dummy method works", "[dummy]") {
-    REQUIRE(noarr::dummy_method() == 42);
+    vector<'x', scalar<float>> v;
+    auto vs = v | resize<'x'>(10); // transform
+    REQUIRE(is_cube<decltype(vs)>::value == true);
+    REQUIRE(vs.size() == 40);
 }
