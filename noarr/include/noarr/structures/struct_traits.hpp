35d268fe7ce254cc0eb505694b9f0d6e61a6f8ca
jiriklepl <jiriklepl@seznam.cz>
2021-04-05 04:24:59 +0200

    CMake has problems compiling Noarr structures #12

--- noarr/include/noarr/structures/struct_traits.hpp
+++ noarr/include/noarr/structures/struct_traits.hpp
@@ -24,16 +24,40 @@ struct _is_dynamic_dimension {
     using type = std::false_type;
 };
 
+template<typename T, std::size_t O, typename = void>
+struct _has_static_offset {
+    using value_type = bool;
+    constexpr static value_type value = false;
+};
+
+template<typename T, std::size_t O>
+struct _has_static_offset<T, O, void_t<decltype(static_cast<std::size_t (T::*)(std::size_t) const>(&T::template offset<O>))>> {
+    using value_type = bool;
+    constexpr static value_type value = true;
+};
+
+template<typename T, typename = void>
+struct _has_dynamic_offset {
+    using value_type = bool;
+    constexpr static value_type value = false;
+};
+
+template<typename T>
+struct _has_dynamic_offset<T, void_t<decltype(static_cast<std::size_t (T::*)(std::size_t) const>(&T::offset))>> {
+    using value_type = bool;
+    constexpr static value_type value = true;
+};
+
 template<typename T>
 struct _is_dynamic_dimension<T, void_t<decltype(std::declval<T>().offset(std::declval<std::size_t>()))>> {
     using type = std::true_type;
 };
 
 template<typename T>
-using is_static_dimension = typename _is_static_dimension<T>::type;
+using is_static_dimension = std::enable_if_t<_has_static_offset<T, 0>::value, typename _is_static_dimension<T>::type>;
 
 template<typename T>
-using is_dynamic_dimension = typename _is_dynamic_dimension<T>::type;
+using is_dynamic_dimension = std::enable_if_t<_has_dynamic_offset<T>::value, typename _is_dynamic_dimension<T>::type>;
 
 template<typename T, typename = void>
 struct _is_point;
@@ -55,7 +79,12 @@ template<typename T>
 using is_cube = typename _is_cube<remove_cvref<T>>::type;
 
 template<typename T>
-struct _is_cube<T, std::enable_if_t<(is_dynamic_dimension<T>::value || std::is_same<typename get_struct_desc_t<T>::dims, dims_impl<>>::value) && tuple_forall<is_cube, typename sub_structures<T>::value_type>::value>> {
+struct _is_cube<T, std::enable_if_t<std::is_same<typename get_struct_desc_t<T>::dims, dims_impl<>>::value && tuple_forall<is_cube, typename sub_structures<T>::value_type>::value>> {
+    using type = std::true_type;
+};
+
+template<typename T>
+struct _is_cube<T, std::enable_if_t<!std::is_same<typename get_struct_desc_t<T>::dims, dims_impl<>>::value && is_dynamic_dimension<T>::value && tuple_forall<is_cube, typename sub_structures<T>::value_type>::value>> {
     using type = std::true_type;
 };
 
