d0f947055db07c42f9b4a5ef034fe9258b9abd6d
CZpets <simunekpetr98@gmail.com>
2021-04-07 23:02:29 +0200

    .

--- noarr/tests/structures/dummy2_test.cpp
+++ noarr/tests/structures/dummy2_test.cpp
@@ -9,20 +9,30 @@
 #include "noarr/structures/io.hpp"
 #include "noarr/structures/struct_traits.hpp"
 
-using namespace noarr;
+//using namespace noarr;
 
 TEST_CASE("Image", "[image]") {
 
-	array<'x', 1920, array<'y', 1080, tuple<'p', scalar<float>, scalar<float>, scalar<float>, scalar<float>>>> image;
-	array<'x', 1920, array<'y', 1080, array<'z', 4, scalar<float>>>> grayscale;
+	//noarr::array<'x', 1920, noarr::array<'y', 1080, noarr::tuple<'p', noarr::scalar<float>, noarr::scalar<float>, noarr::scalar<float>, noarr::scalar<float>>>> image;
+
+	noarr::array<'x', 1920, noarr::array<'y', 1080, noarr::array<'p', 4, noarr::scalar<float>>>> grayscale;
+	std::vector<char> my_blob_buffer(grayscale | noarr::get_size{});
+	char* my_blob = (char*)my_blob_buffer.data();
 
 	SECTION("check is_cube") {
-		REQUIRE(is_cube<decltype(grayscale)>::value);
-		REQUIRE(!is_cube<decltype(image)>::value);
+		REQUIRE(noarr::is_cube<decltype(grayscale)>::value);
+		//REQUIRE(!noarr::is_cube<decltype(image)>::value);
 	}
 
 	SECTION("check TODO") {
-		auto value_ref = image | fix<'x'>(0) | fix<'y'>(0) | fix<'p'>(2);
+		//auto value_ref = image | fix<'x'>(0) | fix<'y'>(0) | fix<'p'>(2);
+		//std::declval
+
+		//REQUIRE((typeid(image | noarr::fix<'x'>(0) | noarr::fix<'y'>(0) | noarr::fix<'p'>(2)).name()) == "float");
+		//auto value_ref = image | noarr::fix<'x'>(0) | noarr::fix<'y'>(0) | noarr::fix<'p'>(2);
+		//std::size_t offset = image | noarr::fixs<'x', 'y', 'p'>(0, 0, 2) | noarr::offset();
+		std::size_t offset = grayscale | noarr::fix<'x'>(0) | noarr::fix<'y'>(0) | noarr::fix<'p'>(2) | noarr::offset();
+		float& value_ref = *((float*)(my_blob + offset));
 		//float& value_ref = image | fix<'x'>(0) | fix<'y'>(0) | fix<'p'>(2) | offset();
 	}
 }
