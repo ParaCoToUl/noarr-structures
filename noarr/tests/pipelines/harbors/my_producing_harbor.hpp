2be0a6c3b5e18e1d5a2afa038987ef351452aa01
Jirka-Mayer <jirkamayer97@gmail.com>
2021-04-12 21:04:07 +0200

    Fixed build issues

--- noarr/tests/pipelines/harbors/my_producing_harbor.hpp
+++ noarr/tests/pipelines/harbors/my_producing_harbor.hpp
@@ -2,6 +2,191 @@
 // #include <future>
 #include <functional>
 
+class untyped_ship {
+public:
+    void* buffer;
+
+protected:
+    // virtual method needed for polymorphism..
+    // TODO: implement this class and add some virtual methods
+    virtual void foo() = 0;
+};
+
+template<typename Structure, typename BufferItem = void>
+class ship : public untyped_ship {
+public:
+    // ...
+
+protected:
+    // virtual method needed for polymorphism..
+    // TODO: implement this class and add some virtual methods
+    void foo() override {};
+};
+
+class untyped_dock {
+public:
+
+    /**
+     * Possible states of the dock
+     */
+    enum state : unsigned char {
+        /**
+         * No ship at the dock
+         */
+        empty = 0,
+
+        /**
+         * A ship has arrived but hasn't been processed yet
+         */
+        arrived = 1,
+
+        /**
+         * The ship has been processed and is ready to leave
+         */
+        processed = 2
+    };
+
+    /**
+     * Returns the state of the dock
+     */
+    state get_state() {
+        if (this->docked_ship == nullptr)
+            return state::empty;
+        if (this->ship_processed)
+            return state::processed;
+        return state::arrived;
+    }
+
+    /**
+     * Set the target dock, to which processed ships are sent
+     */
+    void send_processed_ships_to(untyped_dock* target) {
+        this->ship_target = target;
+    }
+
+    /**
+     * Returns a reference to the docked ship
+     */
+    const untyped_ship& get_untyped_ship() {
+        if (this->docked_ship == nullptr)
+            throw std::runtime_error("Cannot get a ship when none is docked.");
+
+        return *this->docked_ship;
+    }
+
+private:
+    untyped_ship* docked_ship;
+    bool ship_processed;
+    untyped_dock* ship_target;
+};
+
+template<typename Structure, typename BufferItem = void>
+class dock : public untyped_dock {
+public:
+
+    /**
+     * Returns a reference to the docked ship
+     */
+    const ship<Structure, BufferItem>& get_ship() {
+        return dynamic_cast<const ship<Structure, BufferItem>&>(
+            this->get_untyped_ship()
+        );
+    }
+};
+
+class harbor {
+public:
+
+    /**
+     * Called by the scheduler before the pipeline starts running
+     */
+    void scheduler_start() {
+        this->perform_dock_registration();
+    }
+
+    /**
+     * Called by the scheduler when possible,
+     * guaranteed to not be called when another
+     * update of this harbor is still running
+     * 
+     * @param callback Call when the async operation finishes,
+     * pass true if data was advanced and false if nothing happened
+     */
+    void scheduler_update(std::function<void(bool)> callback) {
+        // if the data cannot be advanced, return immediately
+        if (!this->can_advance())
+        {
+            callback(false);
+            return;
+        }
+
+        // if the data can be advanced, do it
+        this->advance([&](){
+            // data has been advanced
+            callback(true);
+        });
+    }
+
+    /**
+     * Called by the scheduler after an update finishes.
+     * Guaranteed to run on the scheduler thread.
+     */
+    void scheduler_post_update(bool data_was_advanced) {
+        if (data_was_advanced)
+            this->post_advance();
+
+        this->send_ships();
+    }
+
+protected:
+
+    /**
+     * This function is called before the pipeline starts
+     * to register all harbor docks
+     */
+    virtual void register_docks(std::function<void(untyped_dock*)> register_dock) = 0;
+
+    /**
+     * Called to test, whether the advance method can be called
+     */
+    virtual bool can_advance() = 0;
+
+    /**
+     * Called to advance the proggress of data through the harbor
+     */
+    virtual void advance(std::function<void()> callback) = 0;
+
+    /**
+     * Called after the data advancement
+     */
+    virtual void post_advance() {
+        //
+    }
+
+private:
+
+    std::vector<untyped_dock*> registered_docks;
+
+    /**
+     * Calls the register_docks method
+     */
+    void perform_dock_registration() {
+        this->registered_docks.clear();
+        
+        this->register_docks([&](untyped_dock* d) {
+            this->registered_docks.push_back(d);
+        });
+    }
+
+    /**
+     * Sends ships that are ready to leave
+     */
+    void send_ships() {
+        // TODO ...
+    }
+};
+
+
 /**
  * Scheduler manages the runtime of a pipeline
  */
@@ -114,178 +299,6 @@ private:
         this->_expecting_callback = false;
     }
 
-};
-
-class untyped_ship {
-public:
-    void* buffer;
-};
-
-template<typename Structure, typename BufferItem = void>
-class ship : public untyped_ship {
-public:
-    // ...
-};
-
-class untyped_dock {
-public:
-
-    /**
-     * Possible states of the dock
-     */
-    enum state : unsigned char {
-        /**
-         * No ship at the dock
-         */
-        empty = 0,
-
-        /**
-         * A ship has arrived but hasn't been processed yet
-         */
-        arrived = 1,
-
-        /**
-         * The ship has been processed and is ready to leave
-         */
-        processed = 2
-    };
-
-    /**
-     * Returns the state of the dock
-     */
-    state get_state() {
-        if (this->docked_ship == nullptr)
-            return state::empty;
-        if (this->ship_processed)
-            return state::processed;
-        return state::arrived;
-    }
-
-    /**
-     * Set the target dock, to which processed ships are sent
-     */
-    void send_processed_ships_to(untyped_dock* target) {
-        this->ship_target = target;
-    }
-
-    /**
-     * Returns a reference to the docked ship
-     */
-    const untyped_ship& get_untyped_ship() {
-        if (this->docked_ship == nullptr)
-            throw std::runtime_error("Cannot get a ship when none is docked.");
-
-        return *this->docked_ship;
-    }
-
-private:
-    untyped_ship* docked_ship;
-    bool ship_processed;
-    untyped_dock* ship_target;
-};
-
-template<typename Structure, typename BufferItem = void>
-class dock : public untyped_dock {
-public:
-
-    /**
-     * Returns a reference to the docked ship
-     */
-    const ship<Structure, BufferItem>& get_ship() {
-        return this->get_untyped_ship();
-    }
-};
-
-class harbor {
-public:
-
-    /**
-     * Called by the scheduler before the pipeline starts running
-     */
-    void scheduler_start() {
-        this->perform_dock_registration();
-    }
-
-    /**
-     * Called by the scheduler when possible,
-     * guaranteed to not be called when another
-     * update of this harbor is still running
-     * 
-     * @param callback Call when the async operation finishes,
-     * pass true if data was advanced and false if nothing happened
-     */
-    void scheduler_update(std::function<void(bool)> callback) {
-        // if the data cannot be advanced, return immediately
-        if (!this->can_advance())
-        {
-            callback(false);
-            return;
-        }
-
-        // if the data can be advanced, do it
-        this->advance([&](){
-            // data has been advanced
-            callback(true);
-        });
-    }
-
-    /**
-     * Called by the scheduler after an update finishes.
-     * Guaranteed to run on the scheduler thread.
-     */
-    void scheduler_post_update(bool data_was_advanced) {
-        if (data_was_advanced)
-            this->post_advance();
-
-        this->send_ships();
-    }
-
-protected:
-
-    /**
-     * This function is called before the pipeline starts
-     * to register all harbor docks
-     */
-    virtual void register_docks(std::function<void(untyped_dock*)> register_dock) = 0;
-
-    /**
-     * Called to test, whether the advance method can be called
-     */
-    virtual bool can_advance() = 0;
-
-    /**
-     * Called to advance the proggress of data through the harbor
-     */
-    virtual void advance(std::function<void()> callback) = 0;
-
-    /**
-     * Called after the data advancement
-     */
-    virtual void post_advance() {
-        //
-    }
-
-private:
-
-    std::vector<untyped_dock*> registered_docks;
-
-    /**
-     * Calls the register_docks method
-     */
-    void perform_dock_registration() {
-        this->registered_docks.clear();
-        
-        this->register_docks([&](untyped_dock* d) {
-            this->registered_docks.push_back(d);
-        });
-    }
-
-    /**
-     * Sends ships that are ready to leave
-     */
-    void send_ships() {
-        // TODO ...
-    }
 };
 
 class my_producing_harbor : public harbor {
