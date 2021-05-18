ac32d547eb944c7d158854d42dda1e52a0f837db
jiriklepl <jiriklepl@seznam.cz>
2021-05-18 20:16:49 +0200

    slightly refactored wrapper

--- noarr/include/noarr/structures/wrapper.hpp
+++ noarr/include/noarr/structures/wrapper.hpp
@@ -14,37 +14,40 @@ struct _is_cube<wrapper<T>> {
 };
 
 template<typename Structure>
-struct wrapper : private Structure {
+class wrapper : private contain<Structure> {
+    using base = contain<Structure>;
+
+public:
     constexpr wrapper() = default;
-    explicit constexpr wrapper(Structure s) : Structure(s) {}
+    explicit constexpr wrapper(Structure s) : base(s) {}
 
     template<char Dim>
     constexpr auto set_length(std::size_t length) const {
-        return wrap(static_cast<const Structure &>(*this) | noarr::set_length<Dim>(length));
+        return wrap(base::template get<0>() | noarr::set_length<Dim>(length));
     }
 
     template<char... Dims, typename... Ts>
     constexpr auto fix(Ts... ts) const {
-        return wrap(static_cast<const Structure &>(*this) | noarr::fix<Dims...>(ts...));
+        return wrap(base::template get<0>() | noarr::fix<Dims...>(ts...));
     }
 
     template<char... Dims, typename... Ts>
     constexpr auto offset(Ts... ts) const {
-        return static_cast<const Structure &>(*this) | noarr::offset<Dims...>(ts...);
+        return base::template get<0>() | noarr::offset<Dims...>(ts...);
     }
 
     template<char Dim>
     constexpr auto get_length() const {
-        return static_cast<const Structure &>(*this) | noarr::get_length<Dim>();
+        return base::template get<0>() | noarr::get_length<Dim>();
     }
 
     constexpr auto get_size() const {
-        return static_cast<const Structure &>(*this) | noarr::get_size();
+        return base::template get<0>() | noarr::get_size();
     }
 
     template<char... Dims, typename V, typename... Ts>
     constexpr decltype(auto) get_at(V *ptr, Ts... ts) const {
-        return static_cast<const Structure &>(*this) | noarr::get_at<Dims...>(ptr, ts...);
+        return base::template get<0>() | noarr::get_at<Dims...>(ptr, ts...);
     }
 };
 
