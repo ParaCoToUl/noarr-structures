436b6dd5bf0375657d9feb564a925236a43c9f5b
CZpets <simunekpetr98@gmail.com>
2021-06-23 22:19:39 +0200

    Matrix tests 3

--- noarr/tests/structures/matrix_test.cpp
+++ noarr/tests/structures/matrix_test.cpp
@@ -15,7 +15,221 @@
 
 enum MatrixDataLayout { Rows = 0, Columns = 1, Zcurve = 2 };
 
-struct MatrixStruct {
+template<MatrixDataLayout layout>
+struct GetMatrixStructreStructure;
+
+template<>
+struct GetMatrixStructreStructure<MatrixDataLayout::Rows>
+{
+	static constexpr auto GetMatrixStructure()
+	{
+		return noarr::vector<'x', noarr::vector<'y', noarr::scalar<int>>>();
+	}
+};
+
+template<>
+struct GetMatrixStructreStructure<MatrixDataLayout::Columns>
+{
+	static constexpr auto GetMatrixStructure()
+	{
+		return noarr::vector<'y', noarr::vector<'x', noarr::scalar<int>>>();
+	}
+};
+
+template<>
+struct GetMatrixStructreStructure<MatrixDataLayout::Zcurve>
+{
+	static constexpr auto GetMatrixStructure()
+	{
+		return noarr::vector<'x', noarr::vector<'y', noarr::scalar<int>>>();
+	}
+};
+
+
+
+
+
+template<typename Structure>
+struct Bag
+{
+private:
+	noarr::wrapper<Structure> layout_;
+	std::unique_ptr<char[]> data_;
+
+public:
+	explicit Bag(Structure s)
+		: layout_(noarr::wrap(s)),
+		data_(std::make_unique<char[]>(layout().get_size())) { }
+
+	constexpr const noarr::wrapper<Structure>& layout() const noexcept { return layout_; }
+
+	// noarr::wrapper<Structure> &layout() noexcept { return layout_; } // this version should reallocate the blob (maybe only if it doesn't fit)
+
+	constexpr char* data() const noexcept { return data_.get(); }
+
+	void clear()
+	{
+		auto size_ = layout().get_size();
+		for (std::size_t i = 0; i < size_; i++)
+			data_[i] = 0;
+	}
+};
+
+template<typename Structure>
+auto GetBag(Structure s)
+{
+	return Bag<Structure>(s);
+}
+
+template<typename Structure>
+auto GetBag(noarr::wrapper<Structure> s)
+{
+	return Bag<Structure>(s.unwrap());
+}
+
+
+template<typename Structure1, typename Structure2>
+void matrix_copy(Bag<Structure1>& matrix1, Bag<Structure2>& matrix2)
+{
+	int x_size = matrix1.layout().template get_length<'x'>();
+	int y_size = matrix1.layout().template get_length<'y'>();
+
+	REQUIRE(x_size == matrix2.layout().template get_length<'x'>());
+	REQUIRE(y_size == matrix2.layout().template get_length<'y'>());
+
+	for (int i = 0; i < x_size; i++)
+		for (int j = 0; j < y_size; j++)
+		{
+			int& value2 = matrix2.layout().template get_at<'x', 'y'>(matrix2.data(), i, j);
+			value2 = matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), i, j);
+		}
+}
+
+template<typename Structure>
+void matrix_transpose(Bag<Structure>& matrix1)
+{
+	int x_size = matrix1.layout().template get_length<'x'>();
+	int y_size = matrix1.layout().template get_length<'y'>();
+
+	REQUIRE(x_size == y_size);
+
+	for (int i = 0; i < x_size; i++)
+		for (int j = i; j < y_size; j++)
+		{
+			int& value1 = matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), i, j);
+			int& value2 = matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), j, i);
+			std::swap(value1, value2);
+		}
+}
+
+template<typename Structure1, typename Structure2, typename Structure3>
+void matrix_add(Bag<Structure1>& matrix1, Bag<Structure2>& matrix2, Bag<Structure3>& matrix3)
+{
+	int x_size = matrix1.layout().template get_length<'x'>();
+	int y_size = matrix1.layout().template get_length<'y'>();
+
+	REQUIRE(x_size == matrix2.layout().template get_length<'x'>());
+	REQUIRE(y_size == matrix2.layout().template get_length<'y'>());
+	REQUIRE(x_size == matrix3.layout().template get_length<'x'>());
+	REQUIRE(y_size == matrix3.layout().template get_length<'y'>());
+
+	for (int i = 0; i < x_size; i++)
+		for (int j = 0; j < y_size; j++)
+		{
+			int& value1 = matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), i, j);
+			int& value2 = matrix2.layout().template get_at<'x', 'y'>(matrix2.data(), i, j);
+			int& value3 = matrix3.layout().template get_at<'x', 'y'>(matrix3.data(), i, j);
+			value3 = value1 + value2;
+		}
+}
+
+template<typename Structure>
+void matrix_scalar_multiplication(Bag<Structure>& matrix1, int scalar)
+{
+	int x_size = matrix1.layout().template get_length<'x'>();
+	int y_size = matrix1.layout().template get_length<'y'>();
+
+	for (int i = 0; i < x_size; i++)
+		for (int j = i; j < y_size; j++)
+			matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), i, j) *= scalar;
+}
+
+template<typename Structure1, typename Structure2, typename Structure3>
+void matrix_multiply(Bag<Structure1>& matrix1, Bag<Structure2>& matrix2, Bag<Structure3>& matrix3)
+{
+	int x1_size = matrix1.layout().template get_length<'x'>();
+	int y1_size = matrix1.layout().template get_length<'y'>();
+	int x2_size = matrix2.layout().template get_length<'x'>();
+	int y2_size = matrix2.layout().template get_length<'y'>();
+	int x3_size = matrix3.layout().template get_length<'x'>();
+	int y3_size = matrix3.layout().template get_length<'y'>();
+
+	REQUIRE(x1_size == y2_size);
+	REQUIRE(y1_size == y3_size);
+	REQUIRE(x2_size == x3_size);
+
+	for (int i = 0; i < x3_size; i++)
+	{
+		for (int j = 0; j < y3_size; j++)
+		{
+			int sum = 0;
+
+			for (int k = 0; k < x1_size; k++)
+			{
+				int& value1 = matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), k, j);
+				int& value2 = matrix2.layout().template get_at<'x', 'y'>(matrix2.data(), i, k);
+				sum += value1 * value2;
+			}
+
+			matrix3.layout().template get_at<'x', 'y'>(matrix3.data(), i, j) = sum;
+		}
+	}
+}
+
+template<MatrixDataLayout layout>
+void matrix_template_test(int size)
+{
+	auto m1 = GetBag(noarr::wrap(GetMatrixStructreStructure<layout>::GetMatrixStructure()).template set_length<'x'>(size).template set_length<'y'>(size));
+	auto m2 = GetBag(noarr::wrap(GetMatrixStructreStructure<layout>::GetMatrixStructure()).template set_length<'x'>(size).template set_length<'y'>(size));
+	auto m3 = GetBag(noarr::wrap(GetMatrixStructreStructure<layout>::GetMatrixStructure()).template set_length<'x'>(size).template set_length<'y'>(size));
+
+	matrix_multiply(m1, m2, m3);
+}
+
+
+void matrix_template_test_runtime(MatrixDataLayout layout, int size)
+{
+	if (layout == MatrixDataLayout::Rows)
+		matrix_template_test<MatrixDataLayout::Rows>(size);
+	else if (layout == MatrixDataLayout::Columns)
+		matrix_template_test<MatrixDataLayout::Columns>(size);
+	else if (layout == MatrixDataLayout::Zcurve)
+		matrix_template_test<MatrixDataLayout::Zcurve>(size);
+}
+
+TEST_CASE("Small matrix multimplication Rows", "[Small matrix multimplication Rows]")
+{
+	matrix_template_test_runtime(MatrixDataLayout::Rows, 10);
+}
+
+TEST_CASE("Small matrix multimplication Columns", "[Small matrix multimplication Columns]")
+{
+	matrix_template_test_runtime(MatrixDataLayout::Columns, 10);
+}
+
+TEST_CASE("Small matrix multimplication Zcurve", "[Small matrix multimplication Zcurve]")
+{
+	matrix_template_test_runtime(MatrixDataLayout::Zcurve, 10);
+}
+
+
+
+
+
+
+// sèítání, násobit matice, násobit scalárem, 
+
+/*struct MatrixStruct {
 	virtual ~MatrixStruct() noexcept = default;
 	virtual MatrixDataLayout GetLayout() = 0;
 };
@@ -53,82 +267,12 @@ static constexpr auto GetImageStructure(ColumnsStruct)
 static constexpr auto GetMatrixStructure(ZcurveStruct)
 {
 	return noarr::vector<'x', noarr::vector<'y', noarr::scalar<int>>>();
-}
+}*/
 
 
 
 
-
-
-
-template<MatrixDataLayout layout>
-struct GetMatrixStructreStructure;
-
-template<>
-struct GetMatrixStructreStructure<MatrixDataLayout::Rows>
-{
-	static constexpr auto GetImageStructure()
-	{
-		return noarr::vector<'x', noarr::vector<'y', noarr::scalar<int>>>();
-	}
-};
-
-template<>
-struct GetMatrixStructreStructure<MatrixDataLayout::Columns>
-{
-	static constexpr auto GetImageStructure()
-	{
-		return noarr::vector<'y', noarr::vector<'x', noarr::scalar<int>>>();
-	}
-};
-
-template<>
-struct GetMatrixStructreStructure<MatrixDataLayout::Zcurve>
-{
-	static constexpr auto GetMatrixStructure()
-	{
-		return noarr::vector<'x', noarr::vector<'y', noarr::scalar<int>>>();
-	}
-};
-
-
-
-
-template<typename Structure>
-struct Bag
-{
-private:
-	noarr::wrapper<Structure> layout_;
-	std::unique_ptr<char[]> data_;
-	MatrixDataLayout dataLayout_;
-
-public:
-	explicit Bag(Structure s, MatrixDataLayout l)
-		: layout_(noarr::wrap(s)),
-		data_(std::make_unique<char[]>(layout().get_size()),
-		matrixDdataLayout_ataLayout_(l) ) { }
-
-	constexpr const noarr::wrapper<Structure> &layout() const noexcept { return layout_; }
-	
-	constexpr char *data() const noexcept { return data_.get(); }
-
-	constexpr MatrixDataLayout dataLayout() const noexcept { return dataLayout_; }
-
-	void clear()
-	{
-		auto size_ = layout().get_size();
-		for (std::size_t i = 0; i < size_; i++)
-			data_[i] = 0;
-	}
-};
-
-template<typename Structure>
-auto GetBag(Structure s, MatrixDataLayout l)
-{
-	return Bag<Structure>(s, l);
-}
-
-template<typename Structure>
+/*template<typename Structure>
 auto GetBag(noarr::wrapper<Structure> s, MatrixDataLayout l)
 {
 	return Bag<Structure>(s.unwrap(), l);
@@ -138,50 +282,13 @@ template<typename Structure, typename layout>
 auto GetMatrix(int x_size, int y_size)
 {
 	return GetBag(noarr::wrap(GetMatrixStructreStructure<layout>::GetImageStructure(), layout).template set_length<'x'>(x_size).template set_length<'y'>(y_size));
-}
+}*/
 
 
 
 
-template<typename Structure1, typename Structure2>
-void matrix_copy(Bag<Structure1>& matrix1, Bag<Structure2>& matrix2)
-{
-	int x_size = matrix1.layout().template get_length<'x'>();
-	int y_size = matrix1.layout().template get_length<'y'>();
 
-	REQUIRE(x_size == matrix2.layout().template get_length<'x'>());
-	REQUIRE(y_size == matrix2.layout().template get_length<'y'>());
-
-	for (int i = 0; i < x_size; i++)
-	{
-		for (int j = 0; j < y_size; j++)
-		{
-			int& value2 = matrix2.layout().template get_at<'x', 'y'>(matrix2.data(), i, j);
-			value2 = matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), i, j);
-		}
-	}
-}
-
-template<typename Structure>
-void matrix_transpose(Bag<Structure>& matrix1)
-{
-	int x_size = matrix1.layout().template get_length<'x'>();
-	int y_size = matrix1.layout().template get_length<'y'>();
-
-	REQUIRE(x_size == y_size);
-
-	for (int i = 0; i < x_size; i++)
-	{
-		for (int j = i; j < y_size; j++)
-		{
-			int& value1 = matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), i, j);
-			int& value2 = matrix1.layout().template get_at<'x', 'y'>(matrix1.data(), j, i);
-			std::swap(value1, value2);
-		}
-	}
-}
-
-template<typename Structure>
+/*template<typename Structure>
 auto matrix_transpose(Bag<Structure>& matrix)
 {
 	MatrixDataLayout layout = matrix.dataLayout();
@@ -193,10 +300,6 @@ auto matrix_transpose(Bag<Structure>& matrix)
 	else if (layout == MatrixDataLayout::Zcurve)
 		return matrix_transpose<MatrixDataLayout::Zcurve>(matrix);
 }
-
-
-
-
 
 template<typename Structure1, typename targetLayout>
 auto matrix_clone(Bag<Structure1>& matrix1)
@@ -240,7 +343,7 @@ auto matrix_clone(Bag<Structure>& matrix, MatrixDataLayout targetLayout)
 		else if (targetLayout == MatrixDataLayout::Zcurve)
 			return matrix_clone<MatrixDataLayout::Zcurve, MatrixDataLayout::Zcurve>(matrix);
 	}
-}
+}*/
 
 
 /*template<MatrixDataLayout layout, std::size_t width, std::size_t height, std::size_t pixel_range = 256>
