c2671045707ded8a0826de496167c42efea1c120
CZpets <simunekpetr98@gmail.com>
2021-07-12 23:54:43 +0200

    z order

--- noarr/tests/structures/matrix_test.cpp
+++ noarr/tests/structures/matrix_test.cpp
@@ -317,9 +317,37 @@ TEST_CASE("Small matrix multimplication Zcurve", "[Small matrix multimplication 
 	matrix_template_test_runtime(MatrixDataLayout::Zcurve, 10);
 }
 
+uint32_t calcZOrder32(uint16_t x, uint16_t y)
+{
+	x = (x | (x << 8)) & 0x00FF00FF;
+	x = (x | (x << 4)) & 0x0F0F0F0F;
+	x = (x | (x << 2)) & 0x33333333;
+	x = (x | (x << 1)) & 0x55555555;
 
+	y = (y | (y << 8)) & 0x00FF00FF;
+	y = (y | (y << 4)) & 0x0F0F0F0F;
+	y = (y | (y << 2)) & 0x33333333;
+	y = (y | (y << 1)) & 0x55555555;
 
+	return x | (y << 1);
+}
 
+uint64_t calcZOrder64(uint32_t x, uint32_t y)
+{
+	x = (x | (x << 16)) & 0x0000FFFF0000FFFF;
+	x = (x | (x << 8)) & 0x00FF00FF00FF00FF;
+	x = (x | (x << 4)) & 0x0F0F0F0F0F0F0F0F;
+	x = (x | (x << 2)) & 0x3333333333333333;
+	x = (x | (x << 1)) & 0x5555555555555555;
+
+	y = (y | (y << 16)) & 0x0000FFFF0000FFFF;
+	y = (y | (y << 8)) & 0x00FF00FF00FF00FF;
+	y = (y | (y << 4)) & 0x0F0F0F0F0F0F0F0F;
+	y = (y | (y << 2)) & 0x3333333333333333;
+	y = (y | (y << 1)) & 0x5555555555555555;
+
+	return x | (y << 1);
+}
 
 
 // s��t�n�, n�sobit matice, n�sobit scal�rem, 
