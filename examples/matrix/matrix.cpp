60df67aac2b0b37dd1cf6aa787c8acbbb3f5413c
CZpets <simunekpetr98@gmail.com>
2021-08-05 22:49:17 +0200

    matrix example comments v0.1

--- examples/matrix/matrix.cpp
+++ examples/matrix/matrix.cpp
@@ -5,30 +5,43 @@
 #include <cassert>
 #include <tuple>
 
+// raw c++ matrix implementation is from now on a "classic matrix"
+// matrix implementation in noarr will be referecend as "noarr matrix"
+// whole example assumes int matrices
+
+// definion of z-curve data stricture
 #include "z_curve.hpp"
+// definitions of basic matrix functions: matrix multiplication, scalar multiplication, copy, matrix transpose and matrix add
 #include "noarr_matrix_functions.hpp"
 
-using matrix_rows = noarr::vector<'x', noarr::vector<'y', noarr::scalar<int>>>;
-using matrix_columns = noarr::vector<'x', noarr::vector<'y', noarr::scalar<int>>>;
-using matrix_zcurve = noarr::z_curve<'x', 'y', noarr::sized_vector<'a', noarr::scalar<int>>>;
+using matrix_rows = noarr::vector<'n', noarr::vector<'m', noarr::scalar<int>>>;
+using matrix_columns = noarr::vector<'n', noarr::vector<'m', noarr::scalar<int>>>;
+using matrix_zcurve = noarr::z_curve<'n', 'm', noarr::sized_vector<'a', noarr::scalar<int>>>;
 
-struct matrix
+// raw c++ structure, which implements matrix ("classic matrix")
+struct classic_matrix
 {
-	matrix(int X, int Y, std::vector<int>&& Ary) : x(X), y(Y), ary(std::move(Ary)) {};
-	matrix(int X, int Y, std::vector<int>& Ary) : x(X), y(Y), ary(Ary) {};
+	// constructors
+	classic_matrix(int X, int Y, std::vector<int>&& Ary) : n(X), m(Y), ary(std::move(Ary)) {};
+	classic_matrix(int X, int Y, std::vector<int>& Ary) : n(X), m(Y), ary(Ary) {};
 
-	int x;
-	int y;
+	// width
+	int n;
+	// heigth
+	int m;
+	// data vector (flattened matrix by rows into std::vector<int>)
 	std::vector<int> ary;
 
-	int& at(int x_, int y_) { return ary[x_ + y_ * x]; }
-	const int& at(int x_, int y_) const { return ary[x_ + y_ * x]; }
+	// element access functions (returns reference into data vector based on input indexes)
+	int& at(int n_, int m_) { return ary[n_ + m_ * n]; }
+	const int& at(int n_, int m_) const { return ary[n_ + m_ * n]; }
 
+	// printing function which prints the whole matrix into the standard output
 	void print()
 	{
-		for (int i = 0; i < x; i++)
+		for (int i = 0; i < n; i++)
 		{
-			for (int j = 0; j < y; j++)
+			for (int j = 0; j < m; j++)
 				std::cout << at(i, j) << " ";
 
 			std::cout << std::endl;
@@ -38,102 +51,119 @@ struct matrix
 	}
 };
 
-matrix get_clasic_matrix(int x, int y)
+// function returng random classic matrix with values in range [0 to 9] with size n x m. 
+classic_matrix get_clasic_matrix(int n, int m)
 {
-	const int length = x * y;
+	// data container inicialization
+	const int length = n * m;
 	std::vector<int> ary;
 
+	// random values generation
 	for (int i = 0; i < length; i++)
 		ary.push_back(rand() % 10);
 
-	return matrix(x, y, std::move(ary));
+	// matrix construction
+	return classic_matrix(n, m, std::move(ary));
 }
 
-bool are_equal_matrices(matrix& m1, matrix& m2)
+// function which compares two classic matrices for value equality
+bool are_equal_classic_matrices(classic_matrix& m1, classic_matrix& m2)
 {
-	if (m1.x != m2.x)
+	// n size must be the same
+	if (m1.n != m2.n)
 		return false;
 
-	if (m1.y != m2.y)
+	// m size must be the same
+	if (m1.m != m2.m)
 		return false;
 
-	const int length = m1.x * m1.y;
-
+	// data container has to be the same size, we will compare values for equality
+	const int length = m1.n * m1.m;
 	for (int i = 0; i < length; i++)
 		if (m1.ary[i] != m2.ary[i])
 			return false;
 
+	// if all tests were passed up until now the matrices have to be value-equal
 	return true;
 }
 
+// function converting noarr matrix to classic matrix
+// it takes source noarr matrix as argument
 template<typename Structure>
-matrix noarr_matrix_to_clasic(noarr::bag<Structure>& matrix1)
+classic_matrix noarr_matrix_to_clasic(noarr::bag<Structure>& source)
 {
-	int x_size = matrix1.structure().template get_length<'x'>();
-	int y_size = matrix1.structure().template get_length<'y'>();
+	// we will cache matrix size values
+	int n_size = source.structure().template get_length<'n'>();
+	int m_size = source.structure().template get_length<'m'>();
 
-	matrix m = get_clasic_matrix(x_size, y_size);
+	// we will allocate target classic matrix
+	classic_matrix target = get_clasic_matrix(n_size, m_size);
 
-	for (int i = 0; i < x_size; i++)
-		for (int j = 0; j < y_size; j++)
-			m.at(i, j) = matrix1.template at<'x', 'y'>(i, j);
+	// we will go through the matrix and copy noarr matrix into a classic matrix
+	for (int i = 0; i < n_size; i++)
+		for (int j = 0; j < m_size; j++)
+			target.at(i, j) = source.template at<'n', 'm'>(i, j);
 
-	return m;
+	return target;
 }
 
+// function converting classic matrix to noarr matrix
+// it takes source classic matrix and target noarr matrix as arguments
 template<typename Structure>
-void clasic_matrix_to_noarr(matrix& m1, noarr::bag<Structure>& matrix1)
+void clasic_matrix_to_noarr(classic_matrix& source, noarr::bag<Structure>& target)
 {
-	int x_size = matrix1.structure().template get_length<'x'>();
-	int y_size = matrix1.structure().template get_length<'y'>();
+	// we will cache matrix size values
+	int n_size = target.structure().template get_length<'n'>();
+	int m_size = target.structure().template get_length<'m'>();
 
-	for (int i = 0; i < x_size; i++)
-		for (int j = 0; j < y_size; j++)
-			matrix1.template at<'x', 'y'>(i, j) = m1.at(i, j);
+	// we will fo through matrix and copy classic matrix into noarr matrix
+	for (int i = 0; i < n_size; i++)
+		for (int j = 0; j < m_size; j++)
+			target.template at<'n', 'm'>(i, j) = source.at(i, j);
 }
 
-void clasic_matrix_multiply(matrix& m1, matrix& m2, matrix& m3)
+// function multiplying classic matrices
+// it takes 2 source classic matrices and returns multiplied matrix
+classic_matrix clasic_matrix_multiply(classic_matrix& matrix1, classic_matrix& matrix2)
 {
-	int x1_size = m1.x;
-	int y1_size = m1.y;
-	int x2_size = m2.x;
-	int y2_size = m2.y;
-	int x3_size = m3.x;
-	int y3_size = m3.y;
+	// we will cache matrix size values
+	int n1_size = matrix1.n;
+	int m1_size = matrix1.m;
+	int n2_size = matrix2.n;
+	int m2_size = matrix2.m;
 
-	assert(x1_size == y2_size);
-	assert(y1_size == y3_size);
-	assert(x2_size == x3_size);
+	// n1 and m2 have to be equal
+	assert(n1_size == m2_size);
 
-	for (int i = 0; i < x3_size; i++)
+	// we will allocate target classic matrix
+	classic_matrix output = get_clasic_matrix(n2_size, m1_size);
+
+	// standart matrix multiplication
+	for (int i = 0; i < n2_size; i++)
 	{
-		for (int j = 0; j < y3_size; j++)
+		for (int j = 0; j < m1_size; j++)
 		{
 			int sum = 0;
 
-			for (int k = 0; k < x1_size; k++)
-			{
-				int& value1 = m1.at(k, j);
-				int& value2 = m2.at(i, k);
+			for (int k = 0; k < n1_size; k++)
+				sum += matrix1.at(k, j) * matrix2.at(i, k);
 
-				sum += value1 * value2;
-			}
-
-			m3.at(i, j) = sum;
+			output.at(i, j) = sum;
 		}
 	}
+
+	return output;
 }
+
 
 template<typename Structure>
 void matrix_demo(int size, Structure structure)
 {
-	matrix m1 = get_clasic_matrix(size, size);
-	matrix m2 = get_clasic_matrix(size, size);
-	matrix m3 = get_clasic_matrix(size, size);
-
+	classic_matrix m1 = get_clasic_matrix(size, size);
 	std::cout << "Matrix 1:" << std::endl;
 	m1.print();
 
+	classic_matrix m2 = get_clasic_matrix(size, size);
 	std::cout << "Matrix 2:" << std::endl;
 	m2.print();
 
@@ -144,18 +174,18 @@ void matrix_demo(int size, Structure structure)
 	clasic_matrix_to_noarr(m1, n1);
 	clasic_matrix_to_noarr(m2, n2);
 
-	clasic_matrix_multiply(m1, m2, m3);
-	matrix_multiply(n1, n2, n3);
-
-	matrix m4 = noarr_matrix_to_clasic(n3);
-
+	classic_matrix m3 = clasic_matrix_multiply(m1, m2);
 	std::cout << "Classic multiplication:" << std::endl;
 	m3.print();
+
+	noarr_matrix_multiply(n1, n2, n3);
+	classic_matrix m4 = noarr_matrix_to_clasic(n3);
+
 
 	std::cout << "Noarr multiplication:" << std::endl;
 	m4.print();
 
-	assert(are_equal_matrices(m3, m4));
+	assert(are_equal_classic_matrices(m3, m4));
 }
 
 int main()
@@ -185,10 +215,10 @@ int main()
 			return -1;
 
 		if (layout == 1)
-			matrix_demo(size, matrix_rows() | noarr::set_length<'x'>(size) | noarr::set_length<'y'>(size));
+			matrix_demo(size, matrix_rows() | noarr::set_length<'n'>(size) | noarr::set_length<'m'>(size));
 		else if (layout == 2)
-			matrix_demo(size, matrix_columns() | noarr::set_length<'x'>(size) | noarr::set_length<'y'>(size));
+			matrix_demo(size, matrix_columns() | noarr::set_length<'n'>(size) | noarr::set_length<'m'>(size));
 		else if (layout == 3)
-			matrix_demo(size, matrix_zcurve(noarr::sized_vector<'a', noarr::scalar<int>>(noarr::scalar<int>(), size * size), noarr::helpers::z_curve_bottom<'x'>(size), noarr::helpers::z_curve_bottom<'y'>(size)));
+			matrix_demo(size, matrix_zcurve(noarr::sized_vector<'a', noarr::scalar<int>>(noarr::scalar<int>(), size * size), noarr::helpers::z_curve_bottom<'n'>(size), noarr::helpers::z_curve_bottom<'m'>(size)));
 	}
 }
--- examples/matrix/noarr_matrix_functions.hpp
+++ examples/matrix/noarr_matrix_functions.hpp
@@ -11,82 +11,82 @@
 #include "noarr/structures/bag.hpp"
 
 template<typename Structure1, typename Structure2>
-void matrix_copy(noarr::bag<Structure1>& matrix1, noarr::bag<Structure2>& matrix2)
+void noarr_matrix_copy(noarr::bag<Structure1>& matrix1, noarr::bag<Structure2>& matrix2)
 {
-	int x_size = matrix1.template get_length<'x'>();
-	int y_size = matrix1.template get_length<'y'>();
+	int x_size = matrix1.template get_length<'n'>();
+	int y_size = matrix1.template get_length<'m'>();
 
-	assert(x_size == matrix2.template get_length<'x'>());
-	assert(y_size == matrix2.template get_length<'y'>());
+	assert(x_size == matrix2.template get_length<'n'>());
+	assert(y_size == matrix2.template get_length<'m'>());
 
 	for (int i = 0; i < x_size; i++)
 		for (int j = 0; j < y_size; j++)
 		{
-			int& value2 = matrix2.template at<'x', 'y'>(i, j);
-			value2 = matrix1.template at<'x', 'y'>(i, j);
+			int& value2 = matrix2.template at<'n', 'm'>(i, j);
+			value2 = matrix1.template at<'n', 'm'>(i, j);
 		}
 }
 
 template<typename Structure>
-void matrix_transpose(noarr::bag<Structure>& matrix1)
+void noarr_matrix_transpose(noarr::bag<Structure>& matrix1)
 {
-	int x_size = matrix1.template get_length<'x'>();
-	int y_size = matrix1.template get_length<'y'>();
+	int x_size = matrix1.template get_length<'n'>();
+	int y_size = matrix1.template get_length<'m'>();
 
 	assert(x_size == y_size);
 
 	for (int i = 0; i < x_size; i++)
 		for (int j = i; j < y_size; j++)
 		{
-			int& value1 = matrix1.template at<'x', 'y'>(i, j);
-			int& value2 = matrix1.template at<'x', 'y'>(j, i);
+			int& value1 = matrix1.template at<'n', 'm'>(i, j);
+			int& value2 = matrix1.template at<'n', 'm'>(j, i);
 
 			std::swap(value1, value2);
 		}
 }
 
 template<typename Structure1, typename Structure2, typename Structure3>
-void matrix_add(noarr::bag<Structure1>& matrix1, noarr::bag<Structure2>& matrix2, noarr::bag<Structure3>& matrix3)
+void noarr_matrix_add(noarr::bag<Structure1>& matrix1, noarr::bag<Structure2>& matrix2, noarr::bag<Structure3>& matrix3)
 {
-	int x_size = matrix1.template get_length<'x'>();
-	int y_size = matrix1.template get_length<'y'>();
+	int x_size = matrix1.template get_length<'n'>();
+	int y_size = matrix1.template get_length<'m'>();
 
-	assert(x_size == matrix2.template get_length<'x'>());
-	assert(y_size == matrix2.template get_length<'y'>());
-	assert(x_size == matrix3.template get_length<'x'>());
-	assert(y_size == matrix3.template get_length<'y'>());
+	assert(x_size == matrix2.template get_length<'n'>());
+	assert(y_size == matrix2.template get_length<'m'>());
+	assert(x_size == matrix3.template get_length<'n'>());
+	assert(y_size == matrix3.template get_length<'m'>());
 
 	for (int i = 0; i < x_size; i++)
 		for (int j = 0; j < y_size; j++)
 		{
-			int& value1 = matrix1.template at<'x', 'y'>(i, j);
-			int& value2 = matrix2.template at<'x', 'y'>(i, j);
-			int& value3 = matrix3.template at<'x', 'y'>(i, j);
+			int& value1 = matrix1.template at<'n', 'm'>(i, j);
+			int& value2 = matrix2.template at<'n', 'm'>(i, j);
+			int& value3 = matrix3.template at<'n', 'm'>(i, j);
 
 			value3 = value1 + value2;
 		}
 }
 
 template<typename Structure>
-void matrix_scalar_multiplication(noarr::bag<Structure>& matrix1, int scalar)
+void noarr_matrix_scalar_multiplication(noarr::bag<Structure>& matrix1, int scalar)
 {
-	int x_size = matrix1.template get_length<'x'>();
-	int y_size = matrix1.template get_length<'y'>();
+	int x_size = matrix1.template get_length<'n'>();
+	int y_size = matrix1.template get_length<'m'>();
 
 	for (int i = 0; i < x_size; i++)
 		for (int j = i; j < y_size; j++)
-			matrix1.template at<'x', 'y'>(i, j) *= scalar;
+			matrix1.template at<'n', 'm'>(i, j) *= scalar;
 }
 
 template<typename Structure1, typename Structure2, typename Structure3>
-void matrix_multiply(noarr::bag<Structure1>& matrix1, noarr::bag<Structure2>& matrix2, noarr::bag<Structure3>& matrix3)
+void noarr_matrix_multiply(noarr::bag<Structure1>& matrix1, noarr::bag<Structure2>& matrix2, noarr::bag<Structure3>& matrix3)
 {
-	int x1_size = matrix1.template get_length<'x'>();
-	int y1_size = matrix1.template get_length<'y'>();
-	int x2_size = matrix2.template get_length<'x'>();
-	int y2_size = matrix2.template get_length<'y'>();
-	int x3_size = matrix3.template get_length<'x'>();
-	int y3_size = matrix3.template get_length<'y'>();
+	int x1_size = matrix1.template get_length<'n'>();
+	int y1_size = matrix1.template get_length<'m'>();
+	int x2_size = matrix2.template get_length<'n'>();
+	int y2_size = matrix2.template get_length<'m'>();
+	int x3_size = matrix3.template get_length<'n'>();
+	int y3_size = matrix3.template get_length<'m'>();
 
 	assert(x1_size == y2_size);
 	assert(y1_size == y3_size);
@@ -100,13 +100,13 @@ void matrix_multiply(noarr::bag<Structure1>& matrix1, noarr::bag<Structure2>& ma
 
 			for (int k = 0; k < x1_size; k++)
 			{
-				int& value1 = matrix1.template at<'x', 'y'>(k, j);
-				int& value2 = matrix2.template at<'x', 'y'>(i, k);
+				int& value1 = matrix1.template at<'n', 'm'>(k, j);
+				int& value2 = matrix2.template at<'n', 'm'>(i, k);
 
 				sum += value1 * value2;
 			}
 
-			matrix3.template at<'x', 'y'>(i, j) = sum;
+			matrix3.template at<'n', 'm'>(i, j) = sum;
 		}
 	}
 }
