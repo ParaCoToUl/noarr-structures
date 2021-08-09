bfb89623551ff457f4ed44900c816e5dbd7d53aa
jiriklepl <jiriklepl@seznam.cz>
2021-08-09 04:41:22 +0200

    literal test split into literal test and pod test; cleaned up

--- noarr/tests/literal_test.cpp
+++ noarr/tests/literal_test.cpp
@@ -1,63 +1,25 @@
 #include <catch2/catch.hpp>
 
-#include <iostream>
-#include <array>
-
 #include "noarr/structures.hpp"
 
-using namespace noarr::literals;
 using namespace noarr;
-
-TEST_CASE("Triviality", "[low-lvl]") {
-    SECTION("array is trivial")
-        REQUIRE(std::is_trivial<array<'x', 100, scalar<int>>>::value);
-
-    SECTION("vector is trivial")
-        REQUIRE(std::is_trivial<vector<'x', scalar<int>>>::value);
-
-    SECTION("sized_vector is trivial")
-        REQUIRE(std::is_trivial<sized_vector<'x', scalar<int>>>::value);
-
-    SECTION("tuple is trivial")
-        REQUIRE(std::is_trivial<tuple<'t', scalar<int>, vector<'x', scalar<int>>, array<'y', 100, scalar<int>>>>::value);
-
-    SECTION("sfixed_dim sized_vector is trivial")
-        REQUIRE(std::is_trivial<sfixed_dim<'x', sized_vector<'x', scalar<int>>, 10>>::value);
-
-    SECTION("fixed_dim sized_vector is trivial")
-        REQUIRE(std::is_trivial<fixed_dim<'x', sized_vector<'x', scalar<int>>>>::value);
-}
-
-TEST_CASE("Standard layout", "[low-lvl]") {
-    SECTION("array is is standard layout")
-        REQUIRE(std::is_standard_layout<array<'x', 100, scalar<int>>>::value);
-
-    SECTION("vector is is standard layout")
-        REQUIRE(std::is_standard_layout<vector<'x', scalar<int>>>::value);
-
-    SECTION("sized_vector is is standard layout")
-        REQUIRE(std::is_standard_layout<sized_vector<'x', scalar<int>>>::value);
-
-    SECTION("tuple is is standard layout")
-        REQUIRE(std::is_standard_layout<tuple<'t', scalar<int>, vector<'x', scalar<int>>, array<'y', 100, scalar<int>>>>::value);
-
-    SECTION("sfixed_dim sized_vector is is standard layout")
-        REQUIRE(std::is_standard_layout<sfixed_dim<'x', sized_vector<'x', scalar<int>>, 10>>::value);
-
-    SECTION("fixed_dim sized_vector is is standard layout")
-        REQUIRE(std::is_standard_layout<fixed_dim<'x', sized_vector<'x', scalar<int>>>>::value);
-}
 
 TEST_CASE("Literality test", "[low-lvl]") {
     constexpr auto test_array = array<'x', 100, scalar<int>>();
-    test_array.size();
+    constexpr auto test_array_size = test_array.size();
+
     constexpr auto test_vector = vector<'x', scalar<int>>();
     constexpr auto test_sized_vector = test_vector | set_length<'x'>(100);
-    test_sized_vector.size();
+    constexpr auto test_sized_vector_size = test_sized_vector.size();
+
     constexpr auto test_tuple = tuple<'t', scalar<int>, sized_vector<'x', scalar<int>>, array<'y', 100, scalar<int>>>();
-    test_tuple.size();
+    constexpr auto test_tuple_size = test_tuple.size();
+
     constexpr auto test_sfixed_dim = sfixed_dim<'x', sized_vector<'x', scalar<int>>, 10>();
-    test_sfixed_dim.size();
+    constexpr auto test_sfixed_dim_size = test_sfixed_dim.size();
+
     constexpr auto test_fixed_dim = fixed_dim<'x', sized_vector<'x', scalar<int>>>();
-    test_fixed_dim.size();
+    constexpr auto test_fixed_dim_size = test_fixed_dim.size();
+
+    REQUIRE(std::integral_constant<std::size_t, (test_array_size + test_sized_vector_size + test_tuple_size + test_sfixed_dim_size + test_fixed_dim_size)>::value == 1204UL);
 }
---
+++ noarr/tests/pod_test.cpp
@@ -0,0 +1,45 @@
+#include <catch2/catch.hpp>
+
+#include "noarr/structures.hpp"
+
+using namespace noarr;
+
+TEST_CASE("Triviality", "[low-lvl]") {
+    SECTION("array is trivial")
+        REQUIRE(std::is_trivial<array<'x', 100, scalar<int>>>::value);
+
+    SECTION("vector is trivial")
+        REQUIRE(std::is_trivial<vector<'x', scalar<int>>>::value);
+
+    SECTION("sized_vector is trivial")
+        REQUIRE(std::is_trivial<sized_vector<'x', scalar<int>>>::value);
+
+    SECTION("tuple is trivial")
+        REQUIRE(std::is_trivial<tuple<'t', scalar<int>, vector<'x', scalar<int>>, array<'y', 100, scalar<int>>>>::value);
+
+    SECTION("sfixed_dim sized_vector is trivial")
+        REQUIRE(std::is_trivial<sfixed_dim<'x', sized_vector<'x', scalar<int>>, 10>>::value);
+
+    SECTION("fixed_dim sized_vector is trivial")
+        REQUIRE(std::is_trivial<fixed_dim<'x', sized_vector<'x', scalar<int>>>>::value);
+}
+
+TEST_CASE("Standard layout", "[low-lvl]") {
+    SECTION("array is is standard layout")
+        REQUIRE(std::is_standard_layout<array<'x', 100, scalar<int>>>::value);
+
+    SECTION("vector is is standard layout")
+        REQUIRE(std::is_standard_layout<vector<'x', scalar<int>>>::value);
+
+    SECTION("sized_vector is is standard layout")
+        REQUIRE(std::is_standard_layout<sized_vector<'x', scalar<int>>>::value);
+
+    SECTION("tuple is is standard layout")
+        REQUIRE(std::is_standard_layout<tuple<'t', scalar<int>, vector<'x', scalar<int>>, array<'y', 100, scalar<int>>>>::value);
+
+    SECTION("sfixed_dim sized_vector is is standard layout")
+        REQUIRE(std::is_standard_layout<sfixed_dim<'x', sized_vector<'x', scalar<int>>, 10>>::value);
+
+    SECTION("fixed_dim sized_vector is is standard layout")
+        REQUIRE(std::is_standard_layout<fixed_dim<'x', sized_vector<'x', scalar<int>>>>::value);
+}
