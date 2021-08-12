4a7f6b80934cd7bf4b001291b981f9e2f3c2ada4
CZpetrs <simunekpetr98@gmail.com>
2021-08-12 18:21:36 +0200

    Create containers_dots_test.cpp

---
+++ noarr/tests/containers_dots_test.cpp
@@ -0,0 +1,92 @@
+#include <catch2/catch.hpp>
+
+#include <array>
+#include <iostream>
+#include <tuple>
+
+#include "noarr/structures_extended.hpp"
+
+TEST_CASE("Vector", "[resizing]")
+{
+	noarr::vector<'x', noarr::scalar<float>> v;
+	auto w = noarr::wrap(v);
+	auto v2 = w.set_length<'x'>(10);
+	
+	SECTION("size check 1") {
+		REQUIRE((v2.get_length<'x'>()) == 10);
+	}
+
+	auto v3 = w.set_length<'x'>(20);
+	auto v4 = v2.set_length<'x'>(30);
+
+	SECTION("size check 2") {
+		REQUIRE((v2.get_length<'x'>()) == 10);
+		REQUIRE((v3.get_length<'x'>()) == 20);
+		REQUIRE((v4.get_length<'x'>()) == 30);
+	}
+
+	SECTION("check is_cube 2") {
+		REQUIRE(!noarr::is_cube<decltype(v)>::value);
+		REQUIRE(noarr::is_cube<decltype(v2)>::value);
+		REQUIRE(noarr::is_cube<decltype(v3)>::value);
+		REQUIRE(noarr::is_cube<decltype(v4)>::value);
+	}
+
+	auto v5 = v4.set_length<'x'>(-10);
+
+	SECTION("check is_cube 3") {
+		REQUIRE(noarr::is_cube<decltype(v5)>::value);
+	}
+}
+
+TEST_CASE("Vector2", "[is_trivial]")
+{
+	noarr::vector<'x', noarr::scalar<float>> v;
+	auto w = noarr::wrap(v);
+	auto v2 = w.set_length<'x'>(10);
+
+	SECTION("is_trivial check 1") {
+		REQUIRE(std::is_trivial<decltype(v2)>::value);
+		REQUIRE(std::is_standard_layout<decltype(v2)>::value);
+	}
+
+	auto v3 = w.set_length<'x'>(20);
+	auto v4 = v2.set_length<'x'>(30);
+
+	SECTION("is_trivial check 2") {
+		REQUIRE(std::is_trivial<decltype(v2)>::value);
+		REQUIRE(std::is_standard_layout<decltype(v2)>::value);
+
+		REQUIRE(std::is_trivial<decltype(v3)>::value);
+		REQUIRE(std::is_standard_layout<decltype(v3)>::value);
+
+		REQUIRE(std::is_trivial<decltype(v4)>::value);
+		REQUIRE(std::is_standard_layout<decltype(v4)>::value);
+	}
+}
+
+TEST_CASE("Array", "[is_trivial]")
+{
+	noarr::array<'x', 1920, noarr::scalar<float>> v;
+	auto w = noarr::wrap(v);
+	auto v2 = w.set_length<'x'>(10);
+
+	SECTION("is_trivial check 1") {
+		REQUIRE(std::is_trivial<decltype(v2)>::value);
+		REQUIRE(std::is_standard_layout<decltype(v2)>::value);
+	}
+
+	auto v3 = w.set_length<'x'>(20);
+	auto v4 = v2.set_length<'x'>(30);
+
+	SECTION("is_trivial check 2") {
+		REQUIRE(std::is_trivial<decltype(v2)>::value);
+		REQUIRE(std::is_standard_layout<decltype(v2)>::value);
+
+		REQUIRE(std::is_trivial<decltype(v3)>::value);
+		REQUIRE(std::is_standard_layout<decltype(v3)>::value);
+
+		REQUIRE(std::is_trivial<decltype(v4)>::value);
+		REQUIRE(std::is_standard_layout<decltype(v4)>::value);
+	}
+}
