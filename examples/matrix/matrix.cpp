65d4eedf399b4774ce08dd6229f6dc8dd2d16505
CZpetrs <simunekpetr98@gmail.com>
2021-08-13 01:14:36 +0200

    Update matrix.cpp

--- examples/matrix/matrix.cpp
+++ examples/matrix/matrix.cpp
@@ -19,7 +19,7 @@
 #include "noarr_matrix_functions.hpp"
 
 // definitions of noarr layouts
-using matrix_rows = noarr::vector<'n', noarr::vector<'m', noarr::scalar<int>>>;
+using matrix_rows = noarr::vector<'m', noarr::vector<'n', noarr::scalar<int>>>;
 using matrix_columns = noarr::vector<'n', noarr::vector<'m', noarr::scalar<int>>>;
 using matrix_zcurve = noarr::z_curve<'n', 'm', noarr::sized_vector<'a', noarr::scalar<int>>>;
 
