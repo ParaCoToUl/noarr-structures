2faab61f000a2f33af49fc3709d00092c51c142b
jiriklepl <jiriklepl@seznam.cz>
2021-08-26 00:46:17 +0200

    data in unlifting made const, helpers added to helpers namespace

--- include/noarr/structures/stream_lifting.hpp
+++ include/noarr/structures/stream_lifting.hpp
@@ -20,24 +20,28 @@ constexpr auto stream_lift(std::istream &in, Struct s, char *offset) -> std::ena
 	return in;
 }
 
+namespace helpers {
+
 template<std::size_t I, typename Struct>
 struct stream_lift_static {
-	static constexpr std::istream &stream_lift_(std::istream &in, Struct s, char *offset) {
-		stream_lift_static<I - 1, Struct>::stream_lift_(in, s, offset);
+	static constexpr std::istream &stream_lift(std::istream &in, Struct s, char *offset) {
+		stream_lift_static<I - 1, Struct>::stream_lift(in, s, offset);
 		return stream_lift(in, std::get<I - 1>(s.sub_structures()), offset + s.template offset<I>());
 	}
 };
 
 template<typename Struct>
 struct stream_lift_static<0, Struct> {
-	static constexpr std::istream &stream_lift_(std::istream &in, Struct, char *) {
+	static constexpr std::istream &stream_lift(std::istream &in, Struct, char *) {
 		return in;
 	}
 };
 
+} // namespace helpers
+
 template<typename Struct>
 constexpr auto stream_lift(std::istream &in, Struct s, char *offset) -> std::enable_if_t<!is_dynamic_dimension<Struct>::value && is_static_dimension<Struct>::value, std::istream&>{
-	return stream_lift_static<s.length(), Struct>::stream_lift_(in, s, offset);
+	return helpers::stream_lift_static<s.length(), Struct>::stream_lift(in, s, offset);
 }
 
 template<typename Struct>
@@ -51,7 +55,7 @@ constexpr std::istream &stream_lift(std::istream &in, scalar<Type>, char *offset
 }
 
 template<typename Struct>
-constexpr auto stream_unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<is_dynamic_dimension<Struct>::value, std::ostream&>{
+constexpr auto stream_unlift(std::ostream &out, Struct s, const char *offset) -> std::enable_if_t<is_dynamic_dimension<Struct>::value, std::ostream&>{
 	for (std::size_t i = 0; i < s.length(); ++i) {
 		stream_unlift(out, std::get<0>(s.sub_structures()), offset + s.offset(i));
 	}
@@ -59,34 +63,38 @@ constexpr auto stream_unlift(std::ostream &out, Struct s, char *offset) -> std::
 	return out;
 }
 
+namespace helpers {
+
 template<std::size_t I, typename Struct>
 struct stream_unlift_static {
-	static constexpr std::ostream &stream_unlift_(std::ostream &out, Struct s, char *offset) {
-		stream_unlift_static<I - 1, Struct>::stream_unlift_(out, s, offset);
+	static constexpr std::ostream &stream_unlift(std::ostream &out, Struct s, const char *offset) {
+		stream_unlift_static<I - 1, Struct>::stream_unlift(out, s, offset);
 		return stream_unlift(out, std::get<I - 1>(s.sub_structures()), offset + s.template offset<I>());
 	}
 };
 
 template<typename Struct>
 struct stream_unlift_static<0, Struct> {
-	static constexpr std::ostream &stream_unlift_(std::ostream &out, Struct, char *) {
+	static constexpr std::ostream &stream_unlift(std::ostream &out, Struct, const char *) {
 		return out;
 	}
 };
 
+} // namespace helpers
+
 template<typename Struct>
-constexpr auto stream_unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<!is_dynamic_dimension<Struct>::value && is_static_dimension<Struct>::value, std::ostream&>{
-	return stream_unlift_static<s.length(), Struct>::stream_unlift_(out, s, offset);
+constexpr auto stream_unlift(std::ostream &out, Struct s, const char *offset) -> std::enable_if_t<!is_dynamic_dimension<Struct>::value && is_static_dimension<Struct>::value, std::ostream&>{
+	return helpers::stream_unlift_static<s.length(), Struct>::stream_unlift(out, s, offset);
 }
 
 template<typename Struct>
-constexpr auto stream_unlift(std::ostream &out, Struct s, char *offset) -> std::enable_if_t<is_point<Struct>::value, std::ostream&>{
+constexpr auto stream_unlift(std::ostream &out, Struct s, const char *offset) -> std::enable_if_t<is_point<Struct>::value, std::ostream&>{
 	return stream_unlift(out, std::get<0>(s.sub_structures()), offset + s.offset());
 }
 
 template<typename Type>
-constexpr std::ostream &stream_unlift(std::ostream &out, scalar<Type>, char *offset) {
-	return out << *reinterpret_cast<Type*>(offset);
+constexpr std::ostream &stream_unlift(std::ostream &out, scalar<Type>, const char *offset) {
+	return out << *reinterpret_cast<const Type*>(offset);
 }
 
 } // namespace noarr
