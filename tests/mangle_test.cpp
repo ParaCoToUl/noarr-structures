cf40bf3ad3f575853d472bf3fc59954e01ef420d
Lukáš Rozsypal <33589083+rqu@users.noreply.github.com>
2022-09-28 03:53:55 +0200

    Fix bugs in mangle, add mangle_test

---
+++ tests/mangle_test.cpp
@@ -0,0 +1,24 @@
+#include <catch2/catch.hpp>
+
+#include "noarr/structures.hpp"
+#include "noarr/structures/mangle.hpp"
+
+#include <string>
+
+template<char... Chars>
+std::string cp2a(noarr::char_pack<Chars...>) { return {Chars...}; }
+
+static std::string int_name = "i" + std::to_string(sizeof(int) * 8);
+static std::string size_name = "u" + std::to_string(sizeof(std::size_t) * 8);
+
+TEST_CASE("Mangle scalar", "[mangle]") {
+	using s = noarr::scalar<int>;
+	using m = noarr::mangle<s>;
+	REQUIRE(cp2a(m()) == "scalar<"+int_name+">");
+}
+
+TEST_CASE("Mangle array", "[mangle]") {
+	using s = noarr::array<'x', 42, noarr::scalar<int>>;
+	using m = noarr::mangle<s>;
+	REQUIRE(cp2a(m()) == "array<'x',("+size_name+")42,scalar<"+int_name+">>");
+}
