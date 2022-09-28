6afdcf4eb4cf992f33d6a924e84aa1bbe3439ac1
Lukáš Rozsypal <33589083+rqu@users.noreply.github.com>
2022-09-28 03:53:55 +0200

    Fix bugs in mangle, add mangle_test

--- include/noarr/structures/mangle.hpp
+++ include/noarr/structures/mangle.hpp
@@ -22,7 +22,7 @@ struct mangle_desc;
  * @tparam T: the structure
  */
 template<class T>
-using mangle = typename helpers::mangle_desc<T::name, std::make_index_sequence<sizeof(T::name - 1)>, typename T::params>::type;
+using mangle = typename helpers::mangle_desc<T::name, std::make_index_sequence<sizeof(T::name) - 1>, typename T::params>::type;
 
 namespace helpers {
 
@@ -30,27 +30,24 @@ template<class T>
 struct mangle_param;
 
 template<class T>
-struct mangle_param<structure_param<T>>
-	: integral_pack_concat<mangle<T>> {};
+struct mangle_param<structure_param<T>> { using type = integral_pack_concat<mangle<T>>; };
 
 template<class T>
-struct mangle_param<type_param<T>>
-	: integral_pack_concat<scalar_name_t<T>> {};
+struct mangle_param<type_param<T>> { using type = integral_pack_concat<scalar_name_t<T>>; };
 
 template<class T, T V>
-struct mangle_param<value_param<T, V>>
-	: integral_pack_concat<char_pack<'('>, scalar_name_t<T>, char_pack<')'>, mangle_value<T, V>> {};
+struct mangle_param<value_param<T, V>> { using type = integral_pack_concat<char_pack<'('>, scalar_name_t<T>, char_pack<')'>, mangle_value<T, V>>; };
 
 template<char Dim>
-struct mangle_param<dim_param<Dim>>
-	: integral_pack_concat<char_pack<'\'', Dim, '\''>> {};
+struct mangle_param<dim_param<Dim>> { using type = integral_pack_concat<char_pack<'\'', Dim, '\''>>; };
 
 template<const char Name[], std::size_t... Indices, class... Params>
-struct mangle_desc<Name, std::index_sequence<Indices...>, struct_params<Params...>>
-	: integral_pack_concat<
+struct mangle_desc<Name, std::index_sequence<Indices...>, struct_params<Params...>> {
+	using type = integral_pack_concat<
 		char_pack<Name[Indices]..., '<'>,
-		integral_pack_concat_sep<char_pack<','>, mangle_param<Params>...>,
-		char_pack<'>'>> {};
+		integral_pack_concat_sep<char_pack<','>, typename mangle_param<Params>::type...>,
+		char_pack<'>'>>;
+};
 
 } // namespace helpers
 
