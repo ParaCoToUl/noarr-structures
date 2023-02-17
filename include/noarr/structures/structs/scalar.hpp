4845defb7a65a33804acfeb83a65ac27798fca47
jiriklepl <jiriklepl@seznam.cz>
2023-02-17 17:08:37 +0100

    make scalar methods static

--- include/noarr/structures/structs/scalar.hpp
+++ include/noarr/structures/structs/scalar.hpp
@@ -25,22 +25,22 @@ struct scalar : contain<> {
 	using signature = scalar_sig<T>;
 
 	template<class State>
-	constexpr auto size(State) const noexcept {
+	static constexpr auto size(State) noexcept {
 		return constexpr_arithmetic::make_const<sizeof(T)>();
 	}
 
 	template<class Sub, class State>
-	constexpr void strict_offset_of(State) const noexcept {
+	static constexpr void strict_offset_of(State) noexcept {
 		static_assert(always_false<Sub>, "Substructure was not found");
 	}
 
 	template<char QDim, class State>
-	constexpr void length(State) const noexcept {
+	static constexpr void length(State) noexcept {
 		static_assert(value_always_false<QDim>, "Index in this dimension is not accepted by any substructure");
 	}
 
 	template<class Sub, class State>
-	constexpr void strict_state_at(State) const noexcept {
+	static constexpr void strict_state_at(State) noexcept {
 		static_assert(always_false<scalar<T>>, "A scalar cannot be used in this context");
 	}
 };
