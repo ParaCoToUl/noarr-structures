485dd66fcf322239dc85bd197b68536b79fd4149
jiriklepl <jiriklepl@seznam.cz>
2023-03-03 10:14:29 +0100

    add (de)serialization for bags

--- include/noarr/structures/interop/serialize_data.hpp
+++ include/noarr/structures/interop/serialize_data.hpp
@@ -5,6 +5,7 @@
 #include <ostream>
 
 #include "../extra/traverser.hpp"
+#include "../interop/bag.hpp"
 
 namespace noarr {
 
@@ -16,12 +17,22 @@ constexpr std::istream &deserialize_data(std::istream &in, Struct s, void *data)
 	return in;
 }
 
+template<class Bag>
+constexpr std::istream &deserialize_data(std::istream &in, Bag &&bag) noexcept {
+	return deserialize_data(in, bag.structure().unwrap(), bag.data());
+}
+
 template<class Struct>
 constexpr std::ostream &serialize_data(std::ostream &out, Struct s, const void *data) noexcept {
 	traverser(s).for_each([&out, s, data](auto state) noexcept {
 		out << (s | get_at(data, state)) << '\n';
 	});
 	return out;
+}
+
+template<class Bag>
+constexpr std::ostream &serialize_data(std::ostream &out, const Bag &bag) noexcept {
+	return serialize_data(out, bag.structure().unwrap(), bag.data());
 }
 
 } // namespace noarr
