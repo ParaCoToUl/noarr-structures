e1da9085c1e7a716521c9bf315cf9827305d7c33
Lukáš Rozsypal <33589083+rqu@users.noreply.github.com>
2023-03-20 02:45:14 +0100

    Add documentation checker to docs_check

---
+++ docs_check/config.py
@@ -0,0 +1,89 @@
+_PROLOG = r'\A'
+_EPILOG = r'\Z'
+_ANY = '(?:.|\n)'
+
+_tmp_mulsc = '''
+auto sc = noarr::scalar<float>() ^ noarr::sized_vector<'i'>(42) ^ noarr::sized_vector<'j'>(42);
+auto sr = noarr::scalar<float>() ^ noarr::sized_vector<'j'>(42) ^ noarr::sized_vector<'i'>(42);
+auto bc = noarr::make_bag(sc, (void*)nullptr);
+auto br = noarr::make_bag(sr, (void*)nullptr);
+'''
+_tmp_x = "noarr::array<'x', 42, noarr::scalar<float>>::signature"
+_tmp_y = _tmp_x.replace('x', 'y')
+
+global_decls = '''
+#include <../tests/noarr_test_cuda_dummy.hpp>
+#include <noarr/structures/interop/cuda_striped.cuh>
+#include <noarr/structures/interop/cuda_traverser.cuh>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+'''
+
+substitutions = {
+	('docs/BasicUsage.md', 1): {'auto': ''},
+	('docs/BasicUsage.md', 15): {'n(rows|cols)': '42'},
+	('docs/BasicUsage.md', 16): {_EPILOG: _tmp_mulsc + 'void UNIQ() { mul_by_scalar(sr, nullptr, 4.2); mul_by_scalar(sc, nullptr, 4.2); }\n'},
+	('docs/BasicUsage.md', 17): {_EPILOG: _tmp_mulsc + 'void UNIQ() { mul_by_scalar(br, 4.2); mul_by_scalar(bc, 4.2); }\n'},
+	('docs/BasicUsage.md', 18): {_EPILOG: _tmp_mulsc + 'void UNIQ() { mul_by_scalar(br, 4.2); mul_by_scalar(bc, 4.2); }\n'},
+	('docs/DefiningStructures.md', 5): {'image': 'UNIQ'},
+	('docs/DefiningStructures.md', 6): {'using signature = \.\.\.;': '', '\.\.\.': '0'},
+	('docs/DimensionKinds.md', 0): {_PROLOG: 'using noarr::lit;', '.*incorrect.*': ''},
+	('docs/DimensionKinds.md', 4): {'.*fails at compile time.*': ''},
+	('docs/DimensionKinds.md', 5): {'.*fails at compile time.*': '', '// option': '{//', '// \.\.\.': '} } } }'},
+	('docs/Signature.md', 9): {'/\*\.\.\.\*/::signature': _tmp_x, _EPILOG: f'static_assert(std::is_same_v<new_sig, {_tmp_y}>);\n', 'WANT_DEAD_CODE': '1'},
+	('docs/State.md', 0): {'1<<i': '1'},
+	('docs/State.md', 2): {'my_state': 's1'},
+	('docs/State.md', 3): {'my_state': 's1'},
+	('docs/Traverser.md', 6): {'.*this fails.*': ''},
+	('docs/Traverser.md', 7): {'.*this fails.*': ''},
+	('docs/Traverser.md', 8): {'(from|a|b)_data': 'std::calloc(400*500, sizeof(float))'},
+	('docs/Traverser.md', 13): {'#pragma omp parallel for': ''},
+	('docs/Traverser.md', 14): {'#pragma omp parallel for': ''},
+	('docs/Traverser.md', 18): {
+		'matrix_data': 'std::calloc(300*400, sizeof(float))',
+		f'(noarr::tbb_reduce_bag{_ANY}*,)({_ANY}*)noarr::tbb_reduce_bag.*,': lambda mo: ''.join(mo.group(1, 2, 1)),
+	},
+	('docs/Traverser.md', 19): {_PROLOG: 'void *values_data = nullptr; std::size_t size = 0;'},
+	('docs/Traverser.md', 21): {'[ab]_data': '(void*)nullptr'},
+	('docs/Traverser.md', 22): {'[ab]_data': '(void*)nullptr'},
+	('docs/other/Functions.md', 0): {_PROLOG: "auto matrix = noarr::scalar<int>() ^ noarr::sized_vector<'x'>(1);", '.*(will not work|not make sense).*': ''},
+	('docs/other/Functions.md', 1): {_PROLOG: "auto matrix = noarr::scalar<int>() ^ noarr::sized_vector<'x'>(1);"},
+	('docs/other/Mangling.md', 0): {'/\*\.\.\.\*/': '0'},
+	('docs/other/SeparateLengths.md', 2): {'.*get_length.*': ''},
+	('docs/other/Serialization.md', 1): {'/\*\.\.\.\*/': 'noarr::tuple<42>()', 'path/to/(src|dest)': '/dev/null', 'return 1': 'std::abort()'},
+	('docs/other/StructureTraits.md', 0): {'State = state<>': 'State = noarr::state<>', '/\*\.\.\.\*/': 'void'},
+	('docs/structs/array.md', 0): {'using array = .*;': 'struct array_t;'},
+	('docs/structs/cuda_step.md', 1): {'cuda_step_grid\(\)': 'step(0, 1024*1024)'},
+	('docs/structs/cuda_step.md', 2): {'cuda_step_block\(\)': 'step(0, 1024*1024)'},
+	('docs/structs/cuda_step.md', 3): {"cuda_step_block<'j'>\(\)": "step<'j'>(0, 1024*1024)"},
+	('docs/structs/cuda_step.md', 4): {"cuda_step_grid<'t'>\(\)": "step<'t'>(0, 1024*1024)"},
+	('docs/structs/into_blocks.md', 2): {'/\*\.\.\.\*/': '[](auto){}'},
+	('docs/structs/into_blocks.md', 5): {'num_elems': '42', 'input_data': 'std::calloc(42, sizeof(float))'},
+	('docs/structs/into_blocks.md', 6): {'num_elems': '42', 'input_data': 'std::calloc(42, sizeof(float))'},
+	('docs/structs/merge_zcurve.md', 0): {', char Dim': ''},
+	('docs/structs/rename.md', 1): {'/\*\.\.\.\*/': 'std::calloc(42, sizeof(float))'},
+	('docs/structs/rename.md', 2): {'/\*\.\.\.\*/': '(void*)nullptr', '^matmul': 'auto UNIQ = (matmul', '^\)': '),0)'},
+	('docs/structs/rename.md', 3): {'/\*\.\.\.\*/': '(void*)nullptr'},
+	('docs/structs/vector.md', 0): {'struct vector;': 'struct vector_t;'},
+	('docs/structs/slice.md', 6): {_PROLOG: '#if __cplusplus >= 202002L\n', 'lit': 'noarr::lit', _EPILOG: '#endif\n'},
+	('docs/structs/tuple.md', 0): {'/\*\.\.\.\*/': 'noarr::scalar<int>'},
+	('docs/structs/tuple.md', 4): {'num_edges': '1024', 'data_ptr': '(void*)nullptr'},
+}
+
+def convert_synopsis(code):
+	match code.replace('noarr::', '').split('\n', 2):
+		case [first, '', rest] if first.startswith('#include'):
+			# the synopsis can use auto in parameters list - do not check it against c++17
+			first = '#if __cplusplus >= 202002L'
+			xfirst = '#endif'
+			# we don't want the definitions in scope for the next snippet - throw them away immediately
+			second = 'namespace UNIQ { typedef void proto;'
+			xsecond = '}'
+			#
+			return '\n'.join([first, second, rest, xsecond, xfirst, ''])
+		case _:
+			return ''
---
+++ docs_check/inner-check.py
@@ -0,0 +1,287 @@
+if __name__ != "__main__":
+	raise Exception('do not import me')
+
+import sys, os, os.path
+import re; Pattern = re.compile
+
+from config import global_decls, substitutions, convert_synopsis
+
+###############
+# Definitions #
+###############
+
+scope_block = lambda f: f()
+
+snippatt = Pattern('\n```([a-z]+)\n(.*?\n)```\n', re.DOTALL)
+headpatt = Pattern('(\n+)(#+) ([^\n]+)(?=(\n+))', re.DOTALL)
+tickpatt = Pattern('`.*?`')
+linkpatt = Pattern(r'\[(.*?)\]\((.*?)\)')
+cudapatt = Pattern('(.*)<<<(.*)>>>(.*)')
+
+anchor_ign_patt = Pattern('[().:<>`]')
+
+def clean_anchor(text):
+	return anchor_ign_patt.sub('', text).replace(' ', '-').lower()
+def clean_anchor_1(text):
+	return anchor_ign_patt.sub('', text).replace(' ', '')
+
+def cuda_replacement(mo):
+	kernel, params, rest = mo.group(1, 2, 3)
+	return f'void UNIQ() {{ snippet_check_kernel_params({params}); {kernel}{rest} }}'
+
+##################
+# Initialization #
+##################
+
+try:
+	[cmd, snipdir, include, *args] = sys.argv
+	if not args: raise Exception
+except:
+	print('Usage:', cmd, '<snippets output dir> <include dir> <markdown files ...>')
+	sys.exit(1)
+
+if os.listdir(snipdir):
+	print(snipdir, '(snippet output dir) is not empty')
+	sys.exit(1)
+
+available = set()
+linked = []
+snipfns = 0
+
+@scope_block
+def _():
+	inc = include
+	if not include.endswith('/'):
+		inc += '/'
+	def rel_include(dn):
+		if not dn.startswith(inc):
+			raise Exception(dn, 'does not start with', inc)
+		return dn[len(inc):]
+	with open(f'{snipdir}/snippet_check_all.hpp', 'x') as f:
+		headers = sorted(f'{rel_include(dn)}/{fn}' for dn, _, fns in os.walk(inc) for fn in fns)
+		if not headers:
+			print('Include directory empty or missing')
+			exit(1)
+		for h in headers:
+			if not h.endswith('.hpp'):
+				continue
+			f.write(f'#include <{h}>\n')
+		f.write(global_decls)
+		f.write('#define UNIQ_A(CTR) UNIQ_##CTR\n')
+		f.write('#define UNIQ_B(CTR) UNIQ_A(CTR)\n')
+		f.write('#define UNIQ UNIQ_B(__COUNTER__)\n')
+		f.write('extern struct snippet_check_ret {} snippet_check_ret;\n')
+		f.write('static void snippet_check_kernel_params(dim3, dim3) {}\n')
+
+########
+# Main #
+########
+
+for file in args:
+	with open(file, newline='') as f:
+		content = f.read()
+
+	##########################
+	# Basic file type checks #
+	##########################
+	if not content:
+		print(file, 'is empty')
+		continue
+	if '\ufeff' in content:
+		print(file, 'contains BOM')
+		continue
+	if '\r' in content:
+		print(file, 'contains CR')
+		continue
+	if content[-1] != '\n':
+		print(file, 'last line misses LF')
+		continue
+
+	#######################################
+	# Separate the snippets from the text #
+	#######################################
+	snippets = [] # tuples of (snippet index in file, byte offset in file, c++ source)
+	template_snippets = [] # same format, but they go in a namespace scope instead of fn scope
+	def replacement(mo):
+		idx = len(snippets) + len(template_snippets)
+		code = mo.group(2)
+		match mo.group(1):
+			case 'cpp':
+				coll = template_snippets if 'template<' in code else snippets
+			case 'cu':
+				code = cudapatt.sub(cuda_replacement, code.replace('__global__', ''))
+				coll = template_snippets if 'template<' in code else snippets
+			case 'hpp' | 'cuh':
+				code = convert_synopsis(code)
+				if not code:
+					print(file, 'contains unparsable synopsis')
+				coll = template_snippets
+			case _:
+				print(file, 'contains unrecognized snippet type', mo.group(1))
+				coll = [] # append to nowhere
+		coll.append((idx, mo.start(2), code))
+		return '\n<!-- BLOCK SNIPPET -->\n'
+	text = snippatt.sub(replacement, content)
+
+	######################################
+	# Write out snippets for compilation #
+	######################################
+	if snippets:
+		@scope_block
+		def _():
+			with open(f"{snipdir}/{file.replace('/', '%')}.cpp", 'x') as f:
+				f.write('#include "snippet_check_all.hpp"\n\n')
+				f.write(f'struct snippet_check_ret &snippet_check_fn_{snipfns}()\n')
+				for idx, off, code in snippets:
+					for p, r in substitutions.get((file, idx), {}).items():
+						code = re.sub(p, r, code, flags=re.MULTILINE)
+					line = content[:off].count('\n') + 1
+					f.write('{\n')
+					f.write(f'#line {line} "{file}" // {idx}\n')
+					f.write(code)
+					f.write(f'#line {1000*idx+1} "GENERATED"\n')
+					f.write('\n')
+				f.write(f'return snippet_check_ret;\n')
+				for _ in snippets:
+					f.write('}\n')
+		snipfns += 1
+	if template_snippets:
+		@scope_block
+		def _():
+			with open(f"{snipdir}/{file.replace('/', '%')}.tmpl.cpp", 'x') as f:
+				f.write('#include "snippet_check_all.hpp"\n\n')
+				f.write('namespace {\n')
+				for idx, off, code in template_snippets:
+					for p, r in substitutions.get((file, idx), {}).items():
+						code = re.sub(p, r, code, flags=re.MULTILINE)
+					line = content[:off].count('\n') + 1
+					f.write('namespace UNIQ {\n')
+					f.write(f'#line {line} "{file}" // {idx}\n')
+					f.write(code)
+					f.write(f'#line {1000*idx+1} "GENERATED"\n')
+					f.write('\n')
+				for _ in template_snippets:
+					f.write('}\n')
+				f.write('} // unnamed namespace\n')
+
+	#############################################################
+	# There should be exactly one H1, located on the first line #
+	#############################################################
+	if not text.startswith('# '):
+		print(file, 'does not start with a H1')
+		continue
+	if '\n# ' in text:
+		print(file, 'contains stray H1')
+
+	#################################
+	# The H1 should match file name #
+	#################################
+	@scope_block
+	def _():
+		h1len = text.index('\n')
+		if text[h1len:][:2] != '\n\n':
+			print(file, 'H1 not followed by empty line')
+		if text[h1len+2] == '\n':
+			print(file, 'H1 followed by too many empty lines')
+		if clean_anchor_1(text[2:h1len]) + '.md' != file.split('/')[-1]:
+			print(file, 'H1 does not match filename')
+
+	##########################
+	# H1 only without anchor #
+	##########################
+	available.add(file)
+
+	####################################
+	# Check and collect other headings #
+	####################################
+	@scope_block
+	def _():
+		headings = headpatt.findall(text)
+		if len(headings) != text.count('\n#'):
+			print(file, 'contains unmatched heading (bug in regex?)')
+		for nl_before, hashes, htext, nl_after in headings:
+			level = len(hashes)
+			if level < 2 or level > 5:
+				print(file, 'contains suspicious heading: level', level)
+			if len(nl_after) != 2:
+				print(file, 'contains bad num of blank lines after', htext)
+			if len(nl_before) != (3 if level==2 else 2):
+				print(file, 'contains bad num of blank lines before', htext)
+			available.add(file + '#' + clean_anchor(htext))
+
+	###########################
+	# Check other blank lines #
+	###########################
+	if '\n\n\n' in text.replace('\n\n\n#', ''):
+		print(file, 'contains doubled blank lines (not followed by a heading)')
+
+	#############################################################
+	# Remove inline snippets so they don't interfere with links #
+	#############################################################
+	text = tickpatt.sub('<!-- SNIPPET -->', text)
+	if '`' in text:
+		print(file, 'contains stray backtick or unmatched snippet')
+		continue
+
+	##########################################################
+	# Remember links (check later, after collecting targets) #
+	##########################################################
+	@scope_block
+	def _():
+		for link in linkpatt.finditer(text):
+			(full, lntext, target) = link.group(0, 1, 2)
+			if (
+				lntext.count('(') != lntext.count(')') or
+				'[' in lntext or ']' in lntext or
+				'(' in target or ')' in target or
+				'[' in target or ']' in target):
+				print(file, 'contains corrupted link', full)
+			is_img = text[link.start() - 1] == '!'
+			linked.append((file, target, is_img))
+
+###############
+# Check links #
+###############
+@scope_block
+def _():
+	for source, target, is_img in linked:
+		if target.startswith('https://'):
+			continue
+
+		if target.startswith('#'):
+			if is_img:
+				print('Invalid image linking to anchor')
+				continue
+			resolved = source + target
+		else:
+			left = source.split('/')
+			left.pop()
+			right = target
+			while right.startswith('../'):
+				left.pop()
+				right = right[3:]
+			left.append(right)
+			resolved = '/'.join(left)
+			del left, right
+
+		if is_img: # file
+			if not os.path.isfile(resolved):
+				print('Unresolved image', dict(source=source, relative=target, absolute=resolved))
+		else: # markdown link
+			if not resolved in available:
+				print('Unresolved', dict(source=source, relative=target, absolute=resolved))
+
+###################
+# Main executable #
+###################
+@scope_block
+def _():
+	with open(f'{snipdir}/main.cpp', 'x') as f:
+		f.write('#include <stdlib.h>\n')
+		f.write('struct snippet_check_ret {} snippet_check_ret;\n')
+		f.write('int main() {\n')
+		for i in range(snipfns):
+			f.write(f'extern struct snippet_check_ret &snippet_check_fn_{i}();\n')
+			f.write(f'if(&snippet_check_fn_{i}() != &snippet_check_ret) abort();\n')
+		f.write('return 0;\n')
+		f.write('}\n')
