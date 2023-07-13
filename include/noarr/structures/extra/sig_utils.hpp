4109acc41c83b2c001d2cbdb3c3e5ee83b546db3
jiriklepl <jiriklepl@seznam.cz>
2023-07-13 14:28:55 +0200

    make sig_find_dim_impl more civilized

--- include/noarr/structures/extra/sig_utils.hpp
+++ include/noarr/structures/extra/sig_utils.hpp
@@ -12,24 +12,24 @@ namespace helpers {
 template<IsDim auto QDim, IsState State, class Signature>
 struct sig_find_dim_impl;
 
+template<IsDim auto QDim, IsState State, IsDim auto Dim, class ArgLength, class RetSig> requires (Dim != QDim)
+struct sig_find_dim_impl<QDim, State, function_sig<Dim, ArgLength, RetSig>> {
+	using type = typename sig_find_dim_impl<QDim, State, RetSig>::type;
+};
 template<IsDim auto QDim, IsState State, class ArgLength, class RetSig>
 struct sig_find_dim_impl<QDim, State, function_sig<QDim, ArgLength, RetSig>> {
 	using type = function_sig<QDim, ArgLength, RetSig>;
 };
-template<IsDim auto QDim, IsState State, class... RetSigs>
-struct sig_find_dim_impl<QDim, State, dep_function_sig<QDim, RetSigs...>> {
-	using type = dep_function_sig<QDim, RetSigs...>;
-};
-template<IsDim auto QDim, IsState State, IsDim auto Dim, class ArgLength, class RetSig>
-struct sig_find_dim_impl<QDim, State, function_sig<Dim, ArgLength, RetSig>> {
-	using type = typename sig_find_dim_impl<QDim, State, RetSig>::type;
-};
-template<IsDim auto QDim, IsState State, IsDim auto Dim, class... RetSigs>
+template<IsDim auto QDim, IsState State, IsDim auto Dim, class... RetSigs> requires (Dim != QDim)
 struct sig_find_dim_impl<QDim, State, dep_function_sig<Dim, RetSigs...>> {
 	static_assert(State::template contains<index_in<Dim>>, "Cannot extract dimension from within tuple");
 	static constexpr std::size_t idx = state_get_t<State, index_in<Dim>>::value;
 	using ret_sig = typename dep_function_sig<Dim, RetSigs...>::template ret_sig<idx>;
 	using type = typename sig_find_dim_impl<QDim, State, ret_sig>::type;
+};
+template<IsDim auto QDim, IsState State, class... RetSigs>
+struct sig_find_dim_impl<QDim, State, dep_function_sig<QDim, RetSigs...>> {
+	using type = dep_function_sig<QDim, RetSigs...>;
 };
 template<IsDim auto QDim, IsState State, class ValueType>
 struct sig_find_dim_impl<QDim, State, scalar_sig<ValueType>> {
