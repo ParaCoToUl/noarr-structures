bc9d4f90916667c8c7595559bf87a18bad77a959
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2024-04-18 02:37:22 +0200

    add traversers.hpp

---
+++ include/noarr/traversers.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file traversers.hpp
+ * @brief This file includes structures_extended.hpp, and adds implementation of traversers and planners, iterator support for them and serialization and deserialization functions.
+ */
+
+#include "structures_extended.hpp"
+
+#include "structures/interop/bag.hpp"
+
+#include "structures/extra/traverser.hpp"
+#include "structures/extra/planner.hpp"
+#include "structures/interop/traverser_iter.hpp"
+#include "structures/interop/planner_iter.hpp"
+
+#include "structures/interop/serialize_data.hpp"
