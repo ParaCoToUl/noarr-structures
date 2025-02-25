31aa7bf27411aa8a7039fdd705894283096eac79
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2025-02-25 10:38:31 +0100

    fix discard warnings in state_test.cpp

--- tests/state_test.cpp
+++ tests/state_test.cpp
@@ -52,8 +52,8 @@ TEST_CASE("State shortcuts lit", "[state]") {
 	STATIC_REQUIRE(std::is_same_v<noarr::state_get_t<decltype(s1), noarr::length_in<'x'>>, std::integral_constant<std::size_t, 10>>);
 	STATIC_REQUIRE(std::is_same_v<noarr::state_get_t<decltype(s1), noarr::length_in<'y'>>, std::integral_constant<std::size_t, 20>>);
 
-	STATIC_REQUIRE((s1.get<noarr::length_in<'x'>>()).value == 10);
-	STATIC_REQUIRE((s1.get<noarr::length_in<'y'>>()).value == 20);
+	STATIC_REQUIRE(s1.get<noarr::length_in<'x'>>() == 10);
+	STATIC_REQUIRE(s1.get<noarr::length_in<'y'>>() == 20);
 
 	const auto s2 = s1 & noarr::idx<'x', 'y'>(noarr::lit<05>, noarr::lit<15>);
 
@@ -65,15 +65,15 @@ TEST_CASE("State shortcuts lit", "[state]") {
 	STATIC_REQUIRE(std::is_same_v<noarr::state_get_t<decltype(s2), noarr::index_in<'x'>>, std::integral_constant<std::size_t, 05>>);
 	STATIC_REQUIRE(std::is_same_v<noarr::state_get_t<decltype(s2), noarr::index_in<'y'>>, std::integral_constant<std::size_t, 15>>);
 
-	STATIC_REQUIRE((s2.get<noarr::length_in<'x'>>()).value == 10);
-	STATIC_REQUIRE((s2.get<noarr::length_in<'y'>>()).value == 20);
-	STATIC_REQUIRE((s2.get<noarr::index_in<'x'>>()).value == 05);
-	STATIC_REQUIRE((s2.get<noarr::index_in<'y'>>()).value == 15);
+	STATIC_REQUIRE(s2.get<noarr::length_in<'x'>>() == 10);
+	STATIC_REQUIRE(s2.get<noarr::length_in<'y'>>() == 20);
+	STATIC_REQUIRE(s2.get<noarr::index_in<'x'>>() == 05);
+	STATIC_REQUIRE(s2.get<noarr::index_in<'y'>>() == 15);
 
 	const auto [s2x, s2y] = noarr::get_indices<'x', 'y'>(s2);
 
-	STATIC_REQUIRE((noarr::get_index<'x'>(s2)).value == 05);
-	STATIC_REQUIRE((noarr::get_index<'y'>(s2)).value == 15);
+	STATIC_REQUIRE(noarr::get_index<'x'>(s2) == 05);
+	STATIC_REQUIRE(noarr::get_index<'y'>(s2) == 15);
 	STATIC_REQUIRE(decltype(s2x)::value == 05);
 	STATIC_REQUIRE(decltype(s2y)::value == 15);
 }
